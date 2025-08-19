2294ccb5f4c19aa14c0c5b1a80a1c0f33a2b0f2e
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-08-19 23:30:47 +0100

    Created Identity User and Data Context

--- src/SportsStatistics.Infrastructure/Persistence/SportsStatisticsDbContext.cs
+++ src/SportsStatistics.Infrastructure/Persistence/SportsStatisticsDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SportsStatistics.Infrastructure.Persistence.Models;
 
 namespace SportsStatistics.Infrastructure.Persistence;
 
 internal sealed class SportsStatisticsDbContext(DbContextOptions<SportsStatisticsDbContext> options)
-    : DbContext(options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
 }
