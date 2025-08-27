65e95499e357a71c92f7e77dbe1b33cd53561477
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-08-27 11:43:30 +0100

    Added classes for use with Results pattern

---
+++ src/SportsStatistics.Shared/Results/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace SportsStatistics.Shared.Results;
+
+/// <summary>
+/// Represents the outcome of an operation, including success or failure and associated result error.
+/// </summary>
+public class Result
+{
+    protected internal Result(bool isSuccess, ResultError error)
+    {
+        if (isSuccess && error != ResultError.None)
+        {
+            throw new InvalidOperationException("Result cannot be both successful and have an error.");
+        }
+
+        if (!isSuccess && error == ResultError.None)
+        {
+            throw new InvalidOperationException("Result cannot be unsuccessful but not have an error.");
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public ResultError Error { get; }
+
+    public static Result Success() => new(true, ResultError.None);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, ResultError.None);
+
+    public static Result Failure(ResultError error) => new(false, error);
+
+    public static Result<TValue> Failure<TValue>(ResultError error) => new(default, false, error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) => value is not null ? Success(value) : Failure<TValue>(ResultError.NullValue);
+}
