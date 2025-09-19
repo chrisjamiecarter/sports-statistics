db4b8cda8dd96ac338c11c554a1f9a6f515c6c7d
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-19 20:00:00 +0100

    Added season entity

---
+++ src/SportsStatistics.Domain/Seasons/Season.cs
@@ -0,0 +1,45 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Seasons;
+
+public sealed class Season : Entity
+{
+    private Season(EntityId id, DateOnly startDate, DateOnly endDate, string? customDisplayName) : base(id)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+        CustomDisplayName = customDisplayName;
+    }
+
+    public DateOnly StartDate { get; private set; }
+
+    public DateOnly EndDate { get; private set; }
+
+    public string Name
+        => !string.IsNullOrWhiteSpace(CustomDisplayName)
+        ? CustomDisplayName
+        : $"{StartDate.Year}/{EndDate.Year}";
+
+    private string? CustomDisplayName { get; set; }
+
+    public bool IsCurrent(DateOnly referenceDate)
+        => referenceDate >= StartDate
+        && referenceDate <= EndDate;
+
+    public static Season Create(DateOnly startDate, DateOnly endDate, string? customDisplayName = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startDate, endDate, nameof(startDate));
+
+        var id = EntityId.Create();
+        return new Season(id, startDate, endDate, customDisplayName);
+    }
+
+    public void Update(DateOnly startDate, DateOnly endDate, string? customDisplayName = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startDate, endDate, nameof(startDate));
+
+        StartDate = startDate;
+        EndDate = endDate;
+        CustomDisplayName = customDisplayName;
+    }
+}
