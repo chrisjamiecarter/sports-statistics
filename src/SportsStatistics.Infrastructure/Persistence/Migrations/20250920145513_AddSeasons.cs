f8585b7680c7db24c0f20b2c54f7462648c802e2
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 15:56:30 +0100

    Added seasons migration

---
+++ src/SportsStatistics.Infrastructure/Persistence/Migrations/20250920145513_AddSeasons.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsStatistics.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeasons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seasons",
+                schema: "sports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seasons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seasons",
+                schema: "sports");
+        }
+    }
+}
