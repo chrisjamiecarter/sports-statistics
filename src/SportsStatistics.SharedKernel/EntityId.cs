913407c423cf08eb53587e7e137a9711046c8619
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 13:01:23 +0100

    Added guid version constraint

--- src/SportsStatistics.SharedKernel/EntityId.cs
+++ src/SportsStatistics.SharedKernel/EntityId.cs
@@ -2,16 +2,30 @@
 
 public readonly record struct EntityId
 {
-    public EntityId(Guid value)
+    private static Error IncorrectVersion => Error.Failure(
+        "EntityId.IncorrectVersion",
+        "ID values must be version 7");
+
+    private EntityId(Guid value)
     {
         ArgumentOutOfRangeException.ThrowIfNotEqual(value.Version, 7, nameof(value));
-        
+
         Value = value;
     }
 
     public Guid Value { get; }
 
-    public static EntityId Create() => new(Guid.CreateVersion7());
-    
+    public static Result<EntityId> Create()
+    {
+        return new EntityId(Guid.CreateVersion7());
+    }
+
+    public static Result<EntityId> FromValue(Guid value)
+    {
+        return value.Version == 7
+            ? new EntityId(value)
+            : Result.Failure<EntityId>(IncorrectVersion);
+    }
+
     public override string ToString() => Value.ToString();
 }
