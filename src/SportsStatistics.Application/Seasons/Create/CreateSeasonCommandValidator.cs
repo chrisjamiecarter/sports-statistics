936a32b8c926de069f0a38031b42520b593eff9a
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 13:00:49 +0100

    Added create season command

---
+++ src/SportsStatistics.Application/Seasons/Create/CreateSeasonCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace SportsStatistics.Application.Seasons.Create;
+
+internal sealed class CreateSeasonCommandValidator : AbstractValidator<CreateSeasonCommand>
+{
+    public CreateSeasonCommandValidator()
+    {
+        RuleFor(c => c.StartDate)
+            .NotEmpty()
+            .LessThan(c => c.EndDate);
+
+        RuleFor(c => c.EndDate)
+            .NotEmpty()
+            .GreaterThan(c => c.StartDate);
+    }
+}
+
