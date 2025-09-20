c29348c0abc2504331659e162f9c0da6bf0878ee
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 12:37:06 +0100

    Added season repository interface

---
+++ src/SportsStatistics.Application/Seasons/ISeasonRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportsStatistics.Domain.Seasons;
+
+namespace SportsStatistics.Application.Seasons;
+
+public interface ISeasonRepository
+{
+    Task<bool> CreateAsync(Season season, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Season season, CancellationToken cancellationToken);
+    Task<List<Season>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Season?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<bool> UpdateAsync(Season season, CancellationToken cancellationToken);
+}
