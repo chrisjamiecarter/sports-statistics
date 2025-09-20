dd5d5e1be4a7900abc13ea0934ee90357a238a7a
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 12:37:17 +0100

    Added season errors

---
+++ src/SportsStatistics.Domain/Seasons/SeasonErrors.cs
@@ -0,0 +1,22 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Seasons;
+
+public static class SeasonErrors
+{
+    public static Error NotCreated(EntityId id) => Error.Failure(
+        "Season.NotCreated",
+        $"The season with the Id = '{id}' was not created.");
+    
+    public static Error NotDeleted(EntityId id) => Error.Failure(
+        "Season.NotDeleted",
+        $"The season with the Id = '{id}' was not deleted.");
+
+    public static Error NotFound(EntityId id) => Error.NotFound(
+        "Season.NotFound",
+        $"The season with the Id = '{id}' was not found.");
+
+    public static Error NotUpdated(EntityId id) => Error.Failure(
+        "Season.NotUpdated",
+        $"The season with the Id = '{id}' was not updated.");
+}
