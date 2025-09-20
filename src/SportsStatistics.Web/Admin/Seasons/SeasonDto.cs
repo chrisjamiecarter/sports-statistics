65cba18943659fd00a1b347998b5912f41024901
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-20 15:56:42 +0100

    Added seasons page

---
+++ src/SportsStatistics.Web/Admin/Seasons/SeasonDto.cs
@@ -0,0 +1,6 @@
+﻿namespace SportsStatistics.Web.Admin.Seasons;
+
+public sealed record SeasonDto(Guid Id,
+                               DateOnly StartDate,
+                               DateOnly EndDate,
+                               string Name);
---
+++ src/SportsStatistics.Web/Admin/Seasons/SeasonDtoMapper.cs
@@ -0,0 +1,17 @@
+﻿using SportsStatistics.Application.Seasons.GetAll;
+
+namespace SportsStatistics.Web.Admin.Seasons;
+
+internal static class SeasonDtoMapper
+{
+    public static SeasonDto ToDto(this SeasonResponse season)
+        => new(season.Id,
+               season.StartDate,
+               season.EndDate,
+               season.Name);
+
+    public static IQueryable<SeasonDto> ToQueryable(this List<SeasonResponse> seasons)
+    {
+        return seasons.Select(ToDto).AsQueryable();
+    }
+}
