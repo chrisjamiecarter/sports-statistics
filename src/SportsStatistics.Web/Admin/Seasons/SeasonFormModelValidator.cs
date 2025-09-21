38c27c2e3d8e99e773a5c9ea74d5883a09b218a4
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-21 19:06:54 +0100

    Implemented seasons page with crud

---
+++ src/SportsStatistics.Web/Admin/Seasons/SeasonFormModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace SportsStatistics.Web.Admin.Seasons;
+
+internal sealed class SeasonFormModelValidator : AbstractValidator<SeasonFormModel>
+{
+    public SeasonFormModelValidator()
+    {
+        RuleFor(c => c.StartDate)
+            .NotEmpty()
+            .Must((model, start) => start < model.EndDate)
+            .WithMessage("'Start Date' must be before 'End Date'.");
+
+        RuleFor(c => c.EndDate)
+            .NotEmpty()
+            .Must((model, end) => end > model.StartDate)
+            .WithMessage("'End Date' must be after 'Start Date'.");
+    }
+}
---
+++ src/SportsStatistics.Web/Admin/Seasons/SeasonMapper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components.Extensions;
+using SportsStatistics.Application.Seasons.Create;
+using SportsStatistics.Application.Seasons.Delete;
+using SportsStatistics.Application.Seasons.GetAll;
+using SportsStatistics.Application.Seasons.Update;
+
+namespace SportsStatistics.Web.Admin.Seasons;
+
+internal static class SeasonMapper
+{
+    public static CreateSeasonCommand ToCreateCommand(this SeasonFormModel season)
+    => new(DateOnly.FromDateTime(season.StartDate.GetValueOrDefault()),
+           DateOnly.FromDateTime(season.EndDate.GetValueOrDefault()));
+
+    public static DeleteSeasonCommand ToDeleteCommand(this SeasonDto season)
+        => new(season.Id);
+
+    public static SeasonDto ToDto(this SeasonResponse season)
+        => new(season.Id,
+               season.StartDate,
+               season.EndDate,
+               season.Name);
+
+    public static SeasonFormModel ToFormModel(this SeasonDto season)
+        => new()
+        {
+            StartDate = season.StartDate.ToDateTime(),
+            EndDate = season.EndDate.ToDateTime(),
+        };
+
+    public static IQueryable<SeasonDto> ToQueryable(this List<SeasonResponse> seasons)
+        => seasons.Select(ToDto).AsQueryable();
+
+    public static UpdateSeasonCommand ToUpdateCommand(this SeasonFormModel season, Guid id)
+        => new(id,
+               season.StartDate.ToDateOnly(),
+               season.EndDate.ToDateOnly());
+
+    public static DateOnly ToDateOnly(this DateTime? dateTime)
+        => DateOnly.FromDateTime(dateTime.GetValueOrDefault());
+}
