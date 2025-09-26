188c3774d3282b1e95a6a514c8db4bf18e148aa0
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-26 20:34:02 +0100

    Updated errors with updated EntityId configuration

--- src/SportsStatistics.Domain/Players/PlayerErrors.cs
+++ src/SportsStatistics.Domain/Players/PlayerErrors.cs
@@ -4,19 +4,23 @@ namespace SportsStatistics.Domain.Players;
 
 public static class PlayerErrors
 {
-    public static Error NotCreated(Guid playerId) => Error.Failure(
+    public static Error InvalidPosition(string position) => Error.Failure(
+        "Player.InvalidPosition",
+        $"A player cannot have a position of '{position}'.");
+
+    public static Error NotCreated(EntityId id) => Error.Failure(
         "Player.NotCreated",
-        $"The player with the Id = '{playerId}' was not created.");
+        $"The player with the Id = '{id}' was not created.");
     
-    public static Error NotDeleted(Guid playerId) => Error.Failure(
+    public static Error NotDeleted(EntityId id) => Error.Failure(
         "Player.NotDeleted",
-        $"The player with the Id = '{playerId}' was not deleted.");
+        $"The player with the Id = '{id}' was not deleted.");
 
-    public static Error NotFound(Guid playerId) => Error.NotFound(
+    public static Error NotFound(EntityId id) => Error.NotFound(
         "Player.NotFound",
-        $"The player with the Id = '{playerId}' was not found.");
+        $"The player with the Id = '{id}' was not found.");
 
-    public static Error NotUpdated(Guid playerId) => Error.Failure(
+    public static Error NotUpdated(EntityId id) => Error.Failure(
         "Player.NotUpdated",
-        $"The player with the Id = '{playerId}' was not updated.");
+        $"The player with the Id = '{id}' was not updated.");
 }
