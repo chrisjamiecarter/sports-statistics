86b0f45bfce85fe281060484e678e3da52280af0
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-26 20:31:39 +0100

    Simplified EntityID and implemented IComparable

--- src/SportsStatistics.SharedKernel/EntityId.cs
+++ src/SportsStatistics.SharedKernel/EntityId.cs
@@ -1,11 +1,7 @@
 ﻿namespace SportsStatistics.SharedKernel;
 
-public readonly record struct EntityId
+public readonly record struct EntityId : IComparable<EntityId>
 {
-    private static Error IncorrectVersion => Error.Failure(
-        "EntityId.IncorrectVersion",
-        "ID values must be version 7");
-
     private EntityId(Guid value)
     {
         Value = value;
@@ -13,16 +9,26 @@ public readonly record struct EntityId
 
     public Guid Value { get; }
 
-    public static Result<EntityId> Create()
+    public static EntityId Create()
     {
         return new EntityId(Guid.CreateVersion7());
     }
 
-    public static Result<EntityId> Create(Guid value)
+    public static EntityId Create(Guid value)
     {
-        return TryParse(value, out var id)
-            ? Result.Success(id)
-            : Result.Failure<EntityId>(IncorrectVersion);
+        if (!TryParse(value, out var id))
+        {
+            throw new ArgumentException("ID values must be version 7", nameof(value));
+        }
+
+        return id;
+    }
+
+    public static EntityId Parse(string value)
+    {
+        return !Guid.TryParse(value, out var guid) || !TryParse(guid, out var id)
+            ? throw new FormatException("ID values must be valid version 7 GUIDs.")
+            : id;
     }
 
     public static bool TryParse(Guid value, out EntityId id)
@@ -35,6 +41,35 @@ public readonly record struct EntityId
 
         id = new EntityId(value);
         return true;
+    }
+    
+    public static bool operator <(EntityId left, EntityId right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(EntityId left, EntityId right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(EntityId left, EntityId right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator >=(EntityId left, EntityId right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
+
+    public static implicit operator Guid(EntityId entityId) => entityId.Value;
+    
+    public static explicit operator EntityId(Guid value) => Create(value);
+
+    public int CompareTo(EntityId other)
+    {
+        return Value.CompareTo(other.Value);
     }
 
     public override string ToString() => Value.ToString();
