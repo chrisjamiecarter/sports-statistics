0e6880409a78368526bcceb316a4be328530cd5f
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-26 20:34:22 +0100

    Updated player to inherit Entity

--- src/SportsStatistics.Domain/Players/Player.cs
+++ src/SportsStatistics.Domain/Players/Player.cs
@@ -2,34 +2,57 @@
 
 namespace SportsStatistics.Domain.Players;
 
-public sealed class Player //: Entity
+public sealed class Player : Entity
 {
-    //private Player(EntityId id) : base(id)
-    //{
-    //    // EF Core.
-    //}
-
-    public Guid Id { get; set; }
-
-    public string Name { get; set; } = string.Empty;
-
-    // TODO: player may not have a squad number assigned.
-    public int SquadNumber { get; set; }
-
-    public string Nationality { get; set; } = string.Empty;
-
-    public DateOnly DateOfBirth { get; set; }
-
-    public Position Position { get; set; } = Position.Unknown;
-
-    public int Age => DateOfBirth.CalculateAge();
-
-    public void Update(string name, int squadNumber, string nationality, DateOnly dateOfBirth, Position position)
+    private Player(EntityId id, string name, int squadNumber, string nationality, DateOnly dateOfBirth, Position position) : base(id)
     {
         Name = name;
         SquadNumber = squadNumber;
         Nationality = nationality;
         DateOfBirth = dateOfBirth;
         Position = position;
+    }
+
+    public string Name { get; private set; } = string.Empty;
+
+    // TODO: player may not have a squad number assigned.
+    public int SquadNumber { get; private set; }
+
+    public string Nationality { get; private set; } = string.Empty;
+
+    public DateOnly DateOfBirth { get; private set; }
+
+    public Position Position { get; private set; } = Position.Unknown;
+
+    public int Age => DateOfBirth.CalculateAge();
+
+    public static Player Create(string name, int squadNumber, string nationality, DateOnly dateOfBirth, Position position)
+    {
+        ValidateAndThrow(name, squadNumber, nationality, dateOfBirth, position);
+
+        return new(EntityId.Create(), name, squadNumber, nationality, dateOfBirth, position);
+    }
+
+    public void Update(string name, int squadNumber, string nationality, DateOnly dateOfBirth, Position position)
+    {
+        ValidateAndThrow(name, squadNumber, nationality, dateOfBirth, position);
+
+        Name = name;
+        SquadNumber = squadNumber;
+        Nationality = nationality;
+        DateOfBirth = dateOfBirth;
+        Position = position ?? throw new ArgumentNullException(nameof(position));
+    }
+
+    private static void ValidateAndThrow(string name, int squadNumber, string nationality, DateOnly dateOfBirth, Position position)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfLessThan(squadNumber, 1, nameof(squadNumber));
+        ArgumentException.ThrowIfNullOrEmpty(nationality);
+        ArgumentOutOfRangeException.ThrowIfLessThan(dateOfBirth, DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-15)), nameof(squadNumber));
+        if (position == Position.Unknown)
+        {
+            throw new ArgumentException("A player cannot have a position of unknown.", nameof(position));
+        }
     }
 }
