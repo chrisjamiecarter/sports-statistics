1828bec22db0823c174f0a2249135ab9df34c468
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-09-26 20:37:02 +0100

    Added delete player command handler tests

---
+++ tests/SportsStatistics.Application.Tests/Players/Delete/DeletePlayerCommandHandlerTests.cs
@@ -0,0 +1,88 @@
+﻿using SportsStatistics.Application.Players;
+using SportsStatistics.Application.Players.Delete;
+using SportsStatistics.Domain.Players;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Application.Tests.Players.Delete;
+
+public class DeletePlayerCommandHandlerTests
+{
+    private static readonly Player BasePlayer = Player.Create("Test Name",
+                                                              1,
+                                                              "Test Nationality",
+                                                              DateOnly.FromDateTime(DateTime.Now.AddYears(-15)),
+                                                              Position.Goalkeeper);
+    private static readonly DeletePlayerCommand BaseCommand = new(BasePlayer.Id.Value);
+
+    private readonly Mock<IPlayerRepository> _repositoryMock;
+    private readonly DeletePlayerCommandHandler _handler;
+
+    public DeletePlayerCommandHandlerTests()
+    {
+        _repositoryMock = new Mock<IPlayerRepository>();
+        _handler = new DeletePlayerCommandHandler(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenPlayerIsDeleted()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Success();
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(BasePlayer);
+
+        _repositoryMock.Setup(r => r.DeleteAsync(It.IsAny<Player>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(true);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.DeleteAsync(It.IsAny<Player>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenPlayerIsNotFound()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Failure(PlayerErrors.NotFound(BasePlayer.Id));
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync((Player?)null);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.DeleteAsync(It.IsAny<Player>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenPlayerIsNotDeleted()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Failure(PlayerErrors.NotDeleted(BasePlayer.Id));
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(BasePlayer);
+
+        _repositoryMock.Setup(r => r.DeleteAsync(It.IsAny<Player>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(false);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.DeleteAsync(It.IsAny<Player>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
