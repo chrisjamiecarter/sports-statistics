8eacb1b6a410ee1642607c6ac1e63230f90aa6ee
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-02 17:07:21 +0100

    Added fixture repository definition

---
+++ src/SportsStatistics.Application/Fixtures/IFixtureRepository.cs
@@ -0,0 +1,8 @@
+﻿using SportsStatistics.Domain.Fixtures;
+
+namespace SportsStatistics.Application.Fixtures;
+
+internal interface IFixtureRepository
+{
+    Task<bool> CreateAsync(Fixture fixture, CancellationToken cancellationToken);
+}
