479f32156a8aeab4f10da064f8b820365e6c3948
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-02 17:07:32 +0100

    Added create fixture command

---
+++ src/SportsStatistics.Application/Fixtures/Create/CreateFixtureCommand.cs
@@ -0,0 +1,7 @@
+﻿using SportsStatistics.Application.Abstractions.Messaging;
+
+namespace SportsStatistics.Application.Fixtures.Create;
+
+public sealed record CreateFixtureCommand(DateTime KickoffTimeUtc,
+                                          Guid CompetitionId,
+                                          string FixtureLocation) : ICommand;
