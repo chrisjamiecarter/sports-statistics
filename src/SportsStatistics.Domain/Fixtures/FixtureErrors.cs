e03dd5a262f9c2ae5a1c8ecd2b1f163ebeca2d96
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-02 17:07:04 +0100

    Added fixture errors

---
+++ src/SportsStatistics.Domain/Fixtures/FixtureErrors.cs
@@ -0,0 +1,18 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Fixtures;
+
+public static class FixtureErrors
+{
+    public static Error InvalidLocation(string location) => Error.Failure(
+        "Fixture.InvalidLocation",
+        $"A fixture cannot have a location of '{location}'.");
+
+    public static Error NotCreated(EntityId id) => Error.Failure(
+        "Fixture.NotCreated",
+        $"The fixture with the Id = '{id}' was not created.");
+
+    public static Error NotFound(EntityId id) => Error.NotFound(
+        "Fixture.NotFound",
+        $"The fixture with the Id = '{id}' was not found.");
+}
