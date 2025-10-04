d7928e38e23b0862ec65bdd087cd64852bcaa110
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-04 14:49:24 +0100

    Added create fixture command validator tests

---
+++ tests/SportsStatistics.Application.Tests/Fixtures/Create/CreateFixtureCommandValidatorTests.cs
@@ -0,0 +1,123 @@
+﻿using FluentValidation.TestHelper;
+using SportsStatistics.Application.Fixtures.Create;
+using SportsStatistics.Domain.Fixtures;
+
+namespace SportsStatistics.Application.Tests.Fixtures.Create;
+
+public class CreateFixtureCommandValidatorTests
+{
+    private static readonly CreateFixtureCommand BaseCommand = new(DateTime.UtcNow,
+                                                                  Guid.CreateVersion7(),
+                                                                  FixtureLocation.Home.Name);
+
+    private readonly CreateFixtureCommandValidator _validator;
+
+    public CreateFixtureCommandValidatorTests()
+    {
+        _validator = new CreateFixtureCommandValidator();
+    }
+
+    [Fact]
+    public async Task Should_NotHaveValidationError_When_IsValid()
+    {
+        // Arrange.
+        var command = BaseCommand;
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_KickoffTimeUtcIsEmpty()
+    {
+        // Arrange.
+        var command = BaseCommand with { KickoffTimeUtc = default };
+        var expectedErrorMessage = "'Kickoff Time Utc' must not be empty.";
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.KickoffTimeUtc)
+              .WithErrorMessage(expectedErrorMessage);
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_CompetitionIdIsEmpty()
+    {
+        // Arrange.
+        var command = BaseCommand with { CompetitionId = default };
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.CompetitionId)
+              .WithErrorMessage("'Competition Id' must not be empty.");
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_CompetitionIdIsNotVersion7()
+    {
+        // Arrange.
+        var command = BaseCommand with { CompetitionId = Guid.NewGuid() };
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.CompetitionId)
+              .WithErrorMessage("'Competition Id' is not in the correct format.");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("    ")]
+    public async Task Should_HaveValidationError_When_FixtureLocationIsEmpty(string fixtureLocation)
+    {
+        // Arrange.
+        var command = BaseCommand with { FixtureLocation = fixtureLocation };
+        var expectedErrorMessage = "'Fixture Location' must not be empty.";
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.FixtureLocation)
+              .WithErrorMessage(expectedErrorMessage);
+    }
+
+    [Theory]
+    [InlineData("Home")]
+    [InlineData("Away")]
+    [InlineData("Neutral")]
+    public async Task Should_NotHaveValidationError_When_FixtureLocationIsValid(string fixtureLocation)
+    {
+        // Arrange.
+        var command = BaseCommand with { FixtureLocation = fixtureLocation };
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_FixtureLocationIsInvalid()
+    {
+        // Arrange.
+        var command = BaseCommand with { FixtureLocation = "The Moon" };
+        var expectedMessage = $"Invalid fixture location. Valid fixture locations: {string.Join(", ", FixtureLocation.All)}.";
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.FixtureLocation)
+              .WithErrorMessage(expectedMessage);
+    }
+}
