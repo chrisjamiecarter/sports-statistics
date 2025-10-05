36b6d58301d7fd81f12917974d1cc8aab9a62ac6
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:53:58 +0100

    Added fixture mapper

---
+++ src/SportsStatistics.Application/Fixtures/GetAll/FixtureMapper.cs
@@ -0,0 +1,18 @@
+﻿using SportsStatistics.Domain.Fixtures;
+
+namespace SportsStatistics.Application.Fixtures.GetAll;
+
+internal static class FixtureMapper
+{
+    public static List<FixtureResponse> ToResponse(this IEnumerable<Fixture> fixtures)
+        => [.. fixtures.Select(ToResponse)];
+
+    public static FixtureResponse ToResponse(this Fixture fixture)
+        => new(fixture.Id.Value,
+               fixture.KickoffTimeUtc,
+               fixture.Competition.Id.Value,
+               fixture.Competition.Name,
+               fixture.Competition.Type.Name,
+               fixture.Location.Name,
+               fixture.Status.Name);
+}
