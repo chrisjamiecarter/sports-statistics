1dc25984730a719688a55e81c87429fb385a0427
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:41:21 +0100

    Added fixture response

---
+++ src/SportsStatistics.Application/Fixtures/GetAll/FixtureResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsStatistics.Application.Fixtures.GetAll;
+
+public sealed record FixtureResponse(Guid Id,
+                                     DateTime KickoffTimeUtc,
+                                     Guid CompetitionId,
+                                     string CompetitionName,
+                                     string CompetitionType,
+                                     string FixtureLocationName,
+                                     string FixtureStatusName);
