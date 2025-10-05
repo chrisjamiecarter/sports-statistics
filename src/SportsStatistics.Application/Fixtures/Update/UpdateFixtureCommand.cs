e59b2daeb3ee647e39cccb724f04136f89f661b2
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 14:15:15 +0100

    Added update fixture command

---
+++ src/SportsStatistics.Application/Fixtures/Update/UpdateFixtureCommand.cs
@@ -0,0 +1,11 @@
+﻿using SportsStatistics.Application.Abstractions.Messaging;
+
+namespace SportsStatistics.Application.Fixtures.Update;
+
+public sealed record UpdateFixtureCommand(Guid Id,
+                                          Guid CompetitionId,
+                                          DateTime KickoffTimeUtc,
+                                          string LocationName,
+                                          int HomeGoals,
+                                          int AwayGoals,
+                                          string StatusName) : ICommand;
