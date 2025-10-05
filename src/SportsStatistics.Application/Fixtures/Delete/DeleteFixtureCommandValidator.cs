a8484e2e240f8de7a64665386a6073d8815d880c
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:21:34 +0100

    Added delete fixture command validator

---
+++ src/SportsStatistics.Application/Fixtures/Delete/DeleteFixtureCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SportsStatistics.Application.Fixtures.Delete;
+
+internal sealed class DeleteFixtureCommandValidator : AbstractValidator<DeleteFixtureCommand>
+{
+    public DeleteFixtureCommandValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty()
+            .Must(guid => guid.Version == 7)
+            .WithMessage("'Id' is not in the correct format.");
+    }
+}
