0b682a3ab56079183fdad3c5dcb5c5a1986db750
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:41:31 +0100

    Added get fixtures query

---
+++ src/SportsStatistics.Application/Fixtures/GetAll/GetFixturesQuery.cs
@@ -0,0 +1,5 @@
+﻿using SportsStatistics.Application.Abstractions.Messaging;
+
+namespace SportsStatistics.Application.Fixtures.GetAll;
+
+public sealed record GetFixturesQuery() : IQuery<List<FixtureResponse>>;
