228a7dbe59057c066506c46c09143eda4ff13b71
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:54:29 +0100

    Added get all fixture query handler

---
+++ src/SportsStatistics.Application/Fixtures/GetAll/GetFixturesQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using SportsStatistics.Application.Abstractions.Messaging;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Application.Fixtures.GetAll;
+
+internal sealed class GetFixturesQueryHandler(IFixtureRepository repository) : IQueryHandler<GetFixturesQuery, List<FixtureResponse>>
+{
+    private readonly IFixtureRepository _repository = repository;
+
+    public async Task<Result<List<FixtureResponse>>> Handle(GetFixturesQuery request, CancellationToken cancellationToken)
+    {
+        var fixtures = await _repository.GetAllAsync(cancellationToken);
+        return fixtures.ToResponse();
+    }
+}
