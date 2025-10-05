8ea4403297cd35aebae353bf2285b0ace59b2da0
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 12:21:49 +0100

    Added required methods to support fixture deletion

--- src/SportsStatistics.Application/Fixtures/IFixtureRepository.cs
+++ src/SportsStatistics.Application/Fixtures/IFixtureRepository.cs
@@ -1,8 +1,11 @@
 ﻿using SportsStatistics.Domain.Fixtures;
+using SportsStatistics.SharedKernel;
 
 namespace SportsStatistics.Application.Fixtures;
 
 public interface IFixtureRepository
 {
     Task<bool> CreateAsync(Fixture fixture, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Fixture fixture, CancellationToken cancellationToken);
+    Task<Fixture?> GetByIdAsync(EntityId id, CancellationToken cancellationToken);
 }
