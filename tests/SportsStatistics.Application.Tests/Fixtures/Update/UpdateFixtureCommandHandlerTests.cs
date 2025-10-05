b2b28bfc4109ce5df81647242c4891b58e396403
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-05 14:48:31 +0100

    Added update fixture command handler tests

---
+++ tests/SportsStatistics.Application.Tests/Fixtures/Update/UpdateFixtureCommandHandlerTests.cs
@@ -0,0 +1,121 @@
+﻿using SportsStatistics.Application.Competitions;
+using SportsStatistics.Application.Fixtures;
+using SportsStatistics.Application.Fixtures.Update;
+using SportsStatistics.Domain.Competitions;
+using SportsStatistics.Domain.Fixtures;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Application.Tests.Fixtures.Update;
+
+public class UpdateFixtureCommandHandlerTests
+{
+    private static readonly Competition BaseCompetition = Competition.Create("Test Competition", CompetitionType.League);
+    private static readonly Fixture BaseFixture = Fixture.Create(DateTime.UtcNow.AddDays(7), BaseCompetition, FixtureLocation.Home);
+    private static readonly UpdateFixtureCommand BaseCommand = new(BaseFixture.Id.Value, BaseCompetition.Id.Value, BaseFixture.KickoffTimeUtc.AddDays(1), FixtureLocation.Neutral.Name, 0, 0, FixtureStatus.Scheduled.Name);
+
+    private readonly Mock<IFixtureRepository> _repositoryMock;
+    private readonly Mock<ICompetitionRepository> _competitionRepositoryMock;
+    private readonly UpdateFixtureCommandHandler _handler;
+
+    public UpdateFixtureCommandHandlerTests()
+    {
+        _repositoryMock = new Mock<IFixtureRepository>();
+        _competitionRepositoryMock = new Mock<ICompetitionRepository>();
+        _handler = new UpdateFixtureCommandHandler(_repositoryMock.Object, _competitionRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenFixtureIsUpdated()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Success();
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(BaseFixture);
+
+        _competitionRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(BaseCompetition);
+
+        _repositoryMock.Setup(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(true);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _competitionRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenFixtureIsNotFound()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Failure(FixtureErrors.NotFound(BaseFixture.Id));
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+               .ReturnsAsync((Fixture?)null);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _competitionRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Never);
+        _repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenCompetitionIsNotFound()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Failure(CompetitionErrors.NotFound(BaseCompetition.Id));
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(BaseFixture);
+
+        _competitionRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync((Competition?)null);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _competitionRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenFixtureIsNotUpdated()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var expected = Result.Failure(FixtureErrors.NotUpdated(BaseFixture.Id));
+
+        _repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(BaseFixture);
+
+        _competitionRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(BaseCompetition);
+
+        _repositoryMock.Setup(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(false);
+
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+        _repositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _competitionRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<EntityId>(), It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Fixture>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
