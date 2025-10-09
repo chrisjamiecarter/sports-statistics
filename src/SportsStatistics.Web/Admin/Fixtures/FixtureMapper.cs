6c8676a3d4a7de3d4bfe2558932aeefc7c56d436
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-09 20:10:32 +0100

    Added fixture mapper

---
+++ src/SportsStatistics.Web/Admin/Fixtures/FixtureMapper.cs
@@ -0,0 +1,15 @@
+﻿using SportsStatistics.Application.Fixtures.GetAll;
+
+namespace SportsStatistics.Web.Admin.Fixtures;
+
+internal static class FixtureMapper
+{
+    public static FixtureDto ToDto(this FixtureResponse fixture)
+        => new(fixture.Id,
+               fixture.KickoffTimeUtc,
+               fixture.LocationName,
+               "TODO");
+
+    public static IQueryable<FixtureDto> ToQueryable(this List<FixtureResponse> fixtures)
+        => fixtures.Select(ToDto).AsQueryable();
+}
