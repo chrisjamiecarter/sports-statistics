fa8ae48b6a31705c653c9f2f8ec6f44623cf32c5
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-09 20:10:21 +0100

    Added fixture dto

---
+++ src/SportsStatistics.Web/Admin/Fixtures/FixtureDto.cs
@@ -0,0 +1,6 @@
+﻿namespace SportsStatistics.Web.Admin.Fixtures;
+
+public sealed record FixtureDto(Guid Id,
+                                DateTime KickoffTimeUtc,
+                                string Location,
+                                string Score);
