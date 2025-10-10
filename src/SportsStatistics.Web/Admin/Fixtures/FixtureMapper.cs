d0250296c62521d44dbc9e1d2c9fadeba5392a21
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-10 20:11:16 +0100

    Added additional fixture mappings

--- src/SportsStatistics.Web/Admin/Fixtures/FixtureMapper.cs
+++ src/SportsStatistics.Web/Admin/Fixtures/FixtureMapper.cs
@@ -1,9 +1,21 @@
-﻿using SportsStatistics.Application.Fixtures.GetAll;
+﻿using SportsStatistics.Application.Fixtures.Create;
+using SportsStatistics.Application.Fixtures.Delete;
+using SportsStatistics.Application.Fixtures.GetAll;
+using SportsStatistics.Application.Fixtures.Update;
+using SportsStatistics.Domain.Fixtures;
 
 namespace SportsStatistics.Web.Admin.Fixtures;
 
 internal static class FixtureMapper
 {
+    public static CreateFixtureCommand ToCreateCommand(this FixtureFormModel fixture)
+        => new(fixture.CompetitionId,
+               fixture.KickoffTimeUtc.GetValueOrDefault(),
+               fixture.Location);
+
+    public static DeleteFixtureCommand ToDeleteCommand(this FixtureDto fixture)
+        => new(fixture.Id);
+
     public static FixtureDto ToDto(this FixtureResponse fixture)
         => new(fixture.Id,
                fixture.KickoffTimeUtc,
@@ -12,4 +24,13 @@ internal static class FixtureMapper
 
     public static IQueryable<FixtureDto> ToQueryable(this List<FixtureResponse> fixtures)
         => fixtures.Select(ToDto).AsQueryable();
+
+    // TODO: Update Command Parameters.
+    public static UpdateFixtureCommand ToUpdateCommand(this FixtureFormModel fixture, Guid id)
+        => new(id,
+               fixture.KickoffTimeUtc.GetValueOrDefault(),
+               fixture.Location,
+               0,
+               0,
+               FixtureStatus.Scheduled.Name);
 }
