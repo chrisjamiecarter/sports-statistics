6d22607f3a790f78491f4a4dd6edb81b55a11969
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-17 15:54:10 +0100

    Updated get competitions query handler tests

--- tests/SportsStatistics.Application.Tests/Competitions/GetAll/GetCompetitionsQueryHandlerTests.cs
+++ tests/SportsStatistics.Application.Tests/Competitions/GetAll/GetCompetitionsQueryHandlerTests.cs
@@ -1,84 +1,83 @@
-﻿//using SportsStatistics.Application.Competitions;
-//using SportsStatistics.Application.Competitions.GetAll;
-//using SportsStatistics.Domain.Competitions;
-//using SportsStatistics.SharedKernel;
+﻿using MockQueryable.Moq;
+using SportsStatistics.Application.Abstractions.Data;
+using SportsStatistics.Application.Competitions.GetAll;
+using SportsStatistics.Domain.Competitions;
+using SportsStatistics.SharedKernel;
 
-//namespace SportsStatistics.Application.Tests.Competitions.GetAll;
+namespace SportsStatistics.Application.Tests.Competitions.GetAll;
 
-//public class GetCompetitionsQueryHandlerTests
-//{
-//    private static readonly GetAllCompetitionsQuery BaseCommand = new();
+public class GetCompetitionsQueryHandlerTests
+{
+    private static readonly GetAllCompetitionsQuery BaseCommand = new();
 
-//    private readonly Mock<ICompetitionRepository> _repositoryMock;
-//    private readonly GetAllCompetitionsQueryHandler _handler;
+    private readonly Mock<IApplicationDbContext> _dbContextMock;
+    private readonly GetAllCompetitionsQueryHandler _handler;
 
-//    public GetCompetitionsQueryHandlerTests()
-//    {
-//        _repositoryMock = new Mock<ICompetitionRepository>();
-//        _handler = new GetAllCompetitionsQueryHandler(_repositoryMock.Object);
-//    }
+    public GetCompetitionsQueryHandlerTests()
+    {
+        _dbContextMock = new Mock<IApplicationDbContext>();
 
-//    [Fact]
-//    public async Task Handle_ShouldReturnSuccess_WhenMany()
-//    {
-//        // Arrange.
-//        var command = BaseCommand;
-//        var competitions = new List<Competition>
-//        {
-//            Competition.Create("Test League", CompetitionType.League),
-//            Competition.Create("Test Cup", CompetitionType.Cup),
-//        };
-//        var expected = Result.Success(competitions.ToResponse());
+        _handler = new GetAllCompetitionsQueryHandler(_dbContextMock.Object);
+    }
 
-//        _repositoryMock.Setup(r => r.GetAllAsync(It.IsAny<CancellationToken>()))
-//                       .ReturnsAsync(competitions);
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenMany()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var competitions = new List<Competition>
+        {
+            Competition.Create(EntityId.Create(), "Test League", CompetitionType.League.Name),
+            Competition.Create(EntityId.Create(), "Test Cup", CompetitionType.Cup.Name),
+        };
+        var expected = Result.Success(competitions.ToResponse());
 
-//        // Act.
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        _dbContextMock.Setup(m => m.Competitions)
+                      .Returns(competitions.BuildMockDbSet().Object);
 
-//        // Assert.
-//        result.ShouldBeEquivalentTo(expected);
-//        _repositoryMock.Verify(r => r.GetAllAsync(It.IsAny<CancellationToken>()), Times.Once);
-//    }
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
 
-//    [Fact]
-//    public async Task Handle_ShouldReturnSuccess_WhenOne()
-//    {
-//        // Arrange.
-//        var command = BaseCommand;
-//        var competitions = new List<Competition>
-//        {
-//            Competition.Create("Test League", CompetitionType.League),
-//        };
-//        var expected = Result.Success(competitions.ToResponse());
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+    }
 
-//        _repositoryMock.Setup(r => r.GetAllAsync(It.IsAny<CancellationToken>()))
-//                       .ReturnsAsync(competitions);
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenOne()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var competitions = new List<Competition>
+        {
+            Competition.Create(EntityId.Create(), "Test League", CompetitionType.League.Name),
+        };
+        var expected = Result.Success(competitions.ToResponse());
 
-//        // Act.
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        _dbContextMock.Setup(m => m.Competitions)
+                      .Returns(competitions.BuildMockDbSet().Object);
 
-//        // Assert.
-//        result.ShouldBeEquivalentTo(expected);
-//        _repositoryMock.Verify(r => r.GetAllAsync(It.IsAny<CancellationToken>()), Times.Once);
-//    }
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
 
-//    [Fact]
-//    public async Task Handle_ShouldReturnSuccess_WhenNone()
-//    {
-//        // Arrange.
-//        var command = BaseCommand;
-//        var competitions = new List<Competition>();
-//        var expected = Result.Success(competitions.ToResponse());
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+    }
 
-//        _repositoryMock.Setup(r => r.GetAllAsync(It.IsAny<CancellationToken>()))
-//                       .ReturnsAsync(competitions);
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenNone()
+    {
+        // Arrange.
+        var command = BaseCommand;
+        var competitions = new List<Competition>();
+        var expected = Result.Success(competitions.ToResponse());
 
-//        // Act.
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        _dbContextMock.Setup(m => m.Competitions)
+                      .Returns(competitions.BuildMockDbSet().Object);
 
-//        // Assert.
-//        result.ShouldBeEquivalentTo(expected);
-//        _repositoryMock.Verify(r => r.GetAllAsync(It.IsAny<CancellationToken>()), Times.Once);
-//    }
-//}
+        // Act.
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert.
+        result.ShouldBeEquivalentTo(expected);
+    }
+}
