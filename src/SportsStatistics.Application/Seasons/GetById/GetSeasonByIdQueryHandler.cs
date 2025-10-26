86fcae994f83d5af827428904d70cdeb48eaf4b6
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-10-26 16:18:51 +0000

    Added get season by id query

---
+++ src/SportsStatistics.Application/Seasons/GetById/GetSeasonByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStatistics.Application.Abstractions.Data;
+using SportsStatistics.Application.Abstractions.Messaging;
+using SportsStatistics.Domain.Seasons;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Application.Seasons.GetById;
+
+internal sealed class GetSeasonByIdQueryHandler(IApplicationDbContext dbContext) : IQueryHandler<GetSeasonByIdQuery, SeasonResponse>
+{
+    private readonly IApplicationDbContext _dbContext = dbContext;
+
+    public async Task<Result<SeasonResponse>> Handle(GetSeasonByIdQuery request, CancellationToken cancellationToken)
+    {
+        var seasonId = EntityId.Create(request.SeasonId);
+
+        var season = await _dbContext.Seasons.AsNoTracking()
+                                             .Where(season => season.Id == seasonId)
+                                             .Select(season => season.ToResponse())
+                                             .SingleOrDefaultAsync(cancellationToken);
+
+        return season is not null
+            ? season
+            : Result.Failure<SeasonResponse>(SeasonErrors.NotFound(seasonId));
+    }
+}
---
+++ src/SportsStatistics.Application/Seasons/GetById/SeasonMapper.cs
@@ -0,0 +1,12 @@
+﻿using SportsStatistics.Domain.Seasons;
+
+namespace SportsStatistics.Application.Seasons.GetById;
+
+internal static class SeasonMapper
+{
+    public static SeasonResponse ToResponse(this Season season)
+        => new(season.Id.Value,
+               season.StartDate,
+               season.EndDate,
+               season.Name);
+}
