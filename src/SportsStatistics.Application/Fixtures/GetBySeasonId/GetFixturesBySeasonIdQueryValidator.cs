af26e2046865d3e4d7ea64cad0e220da25a0af4e
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-04 17:40:17 +0000

    Added get fixtures by season id query validator

---
+++ src/SportsStatistics.Application/Fixtures/GetBySeasonId/GetFixturesBySeasonIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace SportsStatistics.Application.Fixtures.GetBySeasonId;
+
+internal sealed class GetFixturesBySeasonIdQueryValidator : AbstractValidator<GetFixturesBySeasonIdQuery>
+{
+    public GetFixturesBySeasonIdQueryValidator()
+    {
+        RuleFor(c => c.SeasonId)
+            .NotEmpty();
+    }
+}
