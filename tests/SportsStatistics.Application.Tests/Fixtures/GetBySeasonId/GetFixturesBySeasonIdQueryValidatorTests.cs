6c1144c98f7eb38099f2013b83b16e96878e144a
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-04 17:43:17 +0000

    Added get fixtures by season id query validator tests

---
+++ tests/SportsStatistics.Application.Tests/Fixtures/GetBySeasonId/GetFixturesBySeasonIdQueryValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using SportsStatistics.Application.Fixtures.GetBySeasonId;
+
+namespace SportsStatistics.Application.Tests.Fixtures.GetBySeasonId;
+
+public class GetFixturesBySeasonIdQueryValidatorTests
+{
+    private static readonly GetFixturesBySeasonIdQuery BaseCommand = new(Guid.CreateVersion7());
+
+    private readonly GetFixturesBySeasonIdQueryValidator _validator;
+
+    public GetFixturesBySeasonIdQueryValidatorTests()
+    {
+        _validator = new GetFixturesBySeasonIdQueryValidator();
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ShouldNotHaveAnyValidationErrors_WhenCommandIsValid()
+    {
+        // Arrange.
+        var command = BaseCommand;
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ShouldHaveValidationError_WhenSeasonIdIsEmpty()
+    {
+        // Arrange.
+        var command = BaseCommand with { SeasonId = default };
+        var expected = "'Season Id' must not be empty.";
+
+        // Act.
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert.
+        result.ShouldHaveValidationErrorFor(c => c.SeasonId)
+              .WithErrorMessage(expected);
+    }
+}
