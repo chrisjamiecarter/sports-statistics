713e20a6c2895632d6ccd65a6aadfdbb4c889dc1
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-10 18:23:23 +0000

    Added player events domain models

---
+++ src/SportsStatistics.Domain/PlayerEvents/PlayerEvent.cs
@@ -0,0 +1,32 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.PlayerEvents;
+
+public sealed class PlayerEvent : Entity
+{
+    public PlayerEvent(EntityId id, EntityId fixtureId, EntityId playerId, PlayerEventType playerEventType, int minute, DateTime occurredAtUtc) : base(id)
+    {
+        FixtureId = fixtureId;
+        PlayerId = playerId;
+        Type = playerEventType;
+        Minute = minute;
+        OccurredAtUtc = occurredAtUtc;
+    }
+
+    public EntityId FixtureId { get; private set; }
+
+    public EntityId PlayerId { get; private set; }
+
+    public PlayerEventType Type { get; private set; } = PlayerEventType.Unknown;
+
+    public int Minute { get; private set; }
+
+    public DateTime OccurredAtUtc { get; private set; }
+
+    public static PlayerEvent Create(EntityId fixtureId, EntityId playerId, string playerEventTypeName, int minute, DateTime occurredAtUtc)
+    {
+        var playerEventType = PlayerEventType.FromName(playerEventTypeName);
+
+        return new(EntityId.Create(), fixtureId, playerId, playerEventType, minute, occurredAtUtc);
+    }
+}
---
+++ src/SportsStatistics.Domain/PlayerEvents/PlayerEventType.cs
@@ -0,0 +1,46 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.PlayerEvents;
+
+public sealed class PlayerEventType : Enumeration
+{
+    public static readonly PlayerEventType Unknown = new(0, nameof(Unknown));
+    public static readonly PlayerEventType PassSuccess = new(1, nameof(PassSuccess));
+    public static readonly PlayerEventType PassFailure = new(2, nameof(PassFailure));
+    public static readonly PlayerEventType ShotOnTarget = new(3, nameof(ShotOnTarget));
+    public static readonly PlayerEventType ShotOffTarget = new(4, nameof(ShotOffTarget));
+    public static readonly PlayerEventType Goal = new(5, nameof(Goal));
+    public static readonly PlayerEventType GoalAssist = new(6, nameof(GoalAssist));
+    public static readonly PlayerEventType OwnGoal = new(7, nameof(OwnGoal));
+    public static readonly PlayerEventType Save = new(8, nameof(Save));
+    public static readonly PlayerEventType Tackle = new(9, nameof(Tackle));
+    public static readonly PlayerEventType FoulWon = new(10, nameof(FoulWon));
+    public static readonly PlayerEventType FoulConceeded = new(11, nameof(FoulConceeded));
+    public static readonly PlayerEventType YellowCard = new(12, nameof(YellowCard));
+    public static readonly PlayerEventType RedCard = new(13, nameof(RedCard));
+
+    private PlayerEventType(int id, string name) : base(id, name) { }
+
+    public static IReadOnlyCollection<PlayerEventType> All =>
+    [
+        PassSuccess,
+        PassFailure,
+        ShotOnTarget,
+        ShotOffTarget,
+        Goal,
+        GoalAssist,
+        OwnGoal,
+        Save,
+        Tackle,
+        FoulWon,
+        FoulConceeded,
+        YellowCard,
+        RedCard
+    ];
+
+    public static PlayerEventType FromName(string eventType)
+    {
+        return All.SingleOrDefault(t => string.Equals(t.Name, eventType, StringComparison.OrdinalIgnoreCase))
+               ?? Unknown;
+    }
+}
