1b7ec512953e0ca4da9c7c745f43757ec4a4c759
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-11 17:30:17 +0000

    Added substitution event domain models

---
+++ src/SportsStatistics.Domain/MatchTracking/SubstitutionEvents/SubstitutionEvent.cs
@@ -0,0 +1,22 @@
+﻿using SportsStatistics.Domain.MatchTracking.MatchEvents;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.MatchTracking.SubstitutionEvents;
+
+public class SubstitutionEvent : MatchEventBase
+{
+    private SubstitutionEvent(EntityId id, EntityId fixtureId, EntityId playerOutId, EntityId playerInId, int minute, DateTime occurredAtUtc) : base(id, fixtureId, minute, occurredAtUtc)
+    {
+        PlayerOutId = playerOutId;
+        PlayerInId = playerInId;
+    }
+
+    public EntityId PlayerOutId { get; private set; }
+
+    public EntityId PlayerInId { get; private set; }
+
+    public static SubstitutionEvent Create(EntityId fixtureId, EntityId playerOutId, EntityId playerInId, int minute, DateTime occurredAtUtc)
+    {
+        return new SubstitutionEvent(EntityId.Create(), fixtureId, playerOutId, playerInId, minute, occurredAtUtc);
+    }
+}
