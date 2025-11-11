fa8aae8223602a6a7c7b2d9362a789be142080aa
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-11 17:16:21 +0000

    Added match events domain models

---
+++ src/SportsStatistics.Domain/MatchEvents/MatchEvent.cs
@@ -0,0 +1,22 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.MatchEvents;
+
+public sealed class MatchEvent : Entity
+{
+    public MatchEvent(EntityId id, EntityId fixtureId, MatchEventType matchEventType, int minute, DateTime occurredAtUtc) : base(id)
+    {
+        FixtureId = fixtureId;
+        Type = matchEventType;
+        Minute = minute;
+        OccurredAtUtc = occurredAtUtc;
+    }
+
+    public EntityId FixtureId { get; private set; }
+
+    public MatchEventType Type { get; private set; } = MatchEventType.Unknown;
+
+    public int Minute { get; private set; }
+
+    public DateTime OccurredAtUtc { get; private set; }
+}
---
+++ src/SportsStatistics.Domain/MatchEvents/MatchEventType.cs
@@ -0,0 +1,30 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.MatchEvents;
+
+public class MatchEventType : Enumeration
+{
+    public static readonly MatchEventType Unknown = new(0, nameof(Unknown));
+    public static readonly MatchEventType Kickoff = new(1, nameof(Kickoff));
+    public static readonly MatchEventType HalfTime = new(2, nameof(HalfTime));
+    public static readonly MatchEventType FullTime = new(3, nameof(FullTime));
+    public static readonly MatchEventType HomeGoal = new(4, nameof(HomeGoal));
+    public static readonly MatchEventType AwayGoal = new(5, nameof(AwayGoal));
+
+    public MatchEventType(int id, string name) : base(id, name) { }
+
+    public static IReadOnlyCollection<MatchEventType> All =>
+    [
+        Kickoff,
+        HalfTime,
+        FullTime,
+        HomeGoal,
+        AwayGoal
+    ];
+
+    public static MatchEventType FromName(string eventType)
+    {
+        return All.SingleOrDefault(t => string.Equals(t.Name, eventType, StringComparison.OrdinalIgnoreCase))
+               ?? Unknown;
+    }
+}
