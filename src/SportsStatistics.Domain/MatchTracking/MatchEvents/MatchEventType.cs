fbb3671c087740371fbb7db1e94d94a69bd4ccf9
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-12 16:15:41 +0000

    Added additional match event types

--- src/SportsStatistics.Domain/MatchTracking/MatchEvents/MatchEventType.cs
+++ src/SportsStatistics.Domain/MatchTracking/MatchEvents/MatchEventType.cs
@@ -5,19 +5,21 @@ namespace SportsStatistics.Domain.MatchTracking.MatchEvents;
 public class MatchEventType : Enumeration
 {
     public static readonly MatchEventType Unknown = new(0, nameof(Unknown));
-    public static readonly MatchEventType Kickoff = new(1, nameof(Kickoff));
-    public static readonly MatchEventType HalfTime = new(2, nameof(HalfTime));
-    public static readonly MatchEventType FullTime = new(3, nameof(FullTime));
-    public static readonly MatchEventType HomeGoal = new(4, nameof(HomeGoal));
-    public static readonly MatchEventType AwayGoal = new(5, nameof(AwayGoal));
+    public static readonly MatchEventType FirstHalfStarted = new(1, nameof(FirstHalfStarted));
+    public static readonly MatchEventType FirstHalfFinished = new(2, nameof(FirstHalfFinished));
+    public static readonly MatchEventType SecondHalfStarted = new(3, nameof(SecondHalfStarted));
+    public static readonly MatchEventType SecondHalfFinished = new(4, nameof(SecondHalfFinished));
+    public static readonly MatchEventType HomeGoal = new(5, nameof(HomeGoal));
+    public static readonly MatchEventType AwayGoal = new(6, nameof(AwayGoal));
 
     public MatchEventType(int id, string name) : base(id, name) { }
 
     public static IReadOnlyCollection<MatchEventType> All =>
     [
-        Kickoff,
-        HalfTime,
-        FullTime,
+        FirstHalfStarted,
+        FirstHalfFinished,
+        SecondHalfStarted,
+        SecondHalfFinished,
         HomeGoal,
         AwayGoal
     ];
