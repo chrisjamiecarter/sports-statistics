80e88f4759c920357b511d372c7bd7dadbcf3cbc
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-17 17:50:05 +0000

    Removed redundant models

--- src/SportsStatistics.Infrastructure/Models/Mappings/ApplicationUserMapping.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SportsStatistics.Infrastructure.Models.Mappings;
-
-//internal static class ApplicationUserMapping
-//{
-//    public static ApplicationUserDto ToDto(this ApplicationUser user)
-//        => new(user.Id, user.UserName ?? string.Empty, user.Email ?? string.Empty);
-//}
