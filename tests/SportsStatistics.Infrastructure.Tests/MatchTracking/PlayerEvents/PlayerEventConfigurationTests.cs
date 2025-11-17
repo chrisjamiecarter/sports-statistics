ad825c9a889bec9926c4714b551954266e9e897f
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-17 17:51:29 +0000

    Added player event configuration tests

---
+++ tests/SportsStatistics.Infrastructure.Tests/MatchTracking/PlayerEvents/PlayerEventConfigurationTests.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SportsStatistics.Domain.MatchTracking.PlayerEvents;
+using SportsStatistics.Infrastructure.Database;
+using SportsStatistics.Infrastructure.Database.Converters;
+using SportsStatistics.Infrastructure.MatchTracking.PlayerEvents;
+
+namespace SportsStatistics.Infrastructure.Tests.MatchTracking.PlayerEvents;
+
+public class PlayerEventConfigurationTests
+{
+    private readonly IMutableEntityType _entity;
+
+    public PlayerEventConfigurationTests()
+    {
+        var modelBuilder = new ModelBuilder();
+        modelBuilder.ApplyConfiguration(new PlayerEventConfiguration());
+
+        _entity = modelBuilder.Model.FindEntityType(typeof(PlayerEvent))
+            ?? throw new InvalidOperationException($"Unable to find '{nameof(PlayerEvent)}' entity type.");
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldMapToCorrectTableAndSchema()
+    {
+        // Arrange.
+        var expectedSchema = Schemas.PlayerEvents.Schema;
+        var expectedTable = Schemas.PlayerEvents.Table;
+
+        // Act.
+        var resultSchema = _entity.GetSchema();
+        var resultTable = _entity.GetTableName();
+
+        // Assert.
+        resultSchema.ShouldBeEquivalentTo(expectedSchema);
+        resultTable.ShouldBeEquivalentTo(expectedTable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureIdAsPrimaryKey()
+    {
+        // Arrange.
+        var expected = nameof(PlayerEvent.Id);
+
+        // Act.
+        var primaryKey = _entity.FindPrimaryKey();
+
+        // Assert.
+        primaryKey.ShouldNotBeNull();
+        primaryKey.Properties.ShouldHaveSingleItem();
+        primaryKey.Properties[0].Name.ShouldBeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureIdPropertyCorrectly()
+    {
+        // Arrange.
+        var expectedValueConverter = Converters.EntityIdConverter;
+        var expectedIsNullable = false;
+        var expectedValueGenerated = ValueGenerated.Never;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.Id));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.GetValueConverter().ShouldBe(expectedValueConverter);
+        property.IsNullable.ShouldBe(expectedIsNullable);
+        property.ValueGenerated.ShouldBe(expectedValueGenerated);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureFixtureIdPropertyCorrectly()
+    {
+        // Arrange.
+        var expectedValueConverter = Converters.EntityIdConverter;
+        var expectedIsNullable = false;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.FixtureId));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.GetValueConverter().ShouldBe(expectedValueConverter);
+        property.IsNullable.ShouldBe(expectedIsNullable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureMinutePropertyCorrectly()
+    {
+        // Arrange.
+        var expectedIsNullable = false;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.Minute));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.IsNullable.ShouldBe(expectedIsNullable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureOccurredAtUtcPropertyCorrectly()
+    {
+        // Arrange.
+        var expectedIsNullable = false;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.OccurredAtUtc));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.IsNullable.ShouldBe(expectedIsNullable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigurePlayerIdPropertyCorrectly()
+    {
+        // Arrange.
+        var expectedValueConverter = Converters.EntityIdConverter;
+        var expectedIsNullable = false;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.PlayerId));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.GetValueConverter().ShouldBe(expectedValueConverter);
+        property.IsNullable.ShouldBe(expectedIsNullable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureTypePropertyCorrectly()
+    {
+        // Arrange.
+        var expectedValueConverter = Converters.PlayerEventTypeConverter;
+        var expectedMaxLength = PlayerEventType.MaxLength;
+        var expectedIsNullable = false;
+
+        // Act.
+        var property = _entity.FindProperty(nameof(PlayerEvent.Type));
+
+        // Assert.
+        property.ShouldNotBeNull();
+        property.GetValueConverter().ShouldBe(expectedValueConverter);
+        property.GetMaxLength().ShouldBe(expectedMaxLength);
+        property.IsNullable.ShouldBe(expectedIsNullable);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigureFixtureIdAsForeignKey()
+    {
+        // Arrange.
+        var expected = nameof(PlayerEvent.FixtureId);
+
+        // Act.
+        var foreignKey = _entity.GetForeignKeys()
+                                .SingleOrDefault(fk => fk.Properties.Any(p => p.Name == expected));
+
+        // Assert.
+        foreignKey.ShouldNotBeNull();
+        foreignKey.Properties.ShouldHaveSingleItem();
+        foreignKey.Properties[0].Name.ShouldBeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void PlayerEventConfiguration_ShouldConfigurePlayerIdAsForeignKey()
+    {
+        // Arrange.
+        var expected = nameof(PlayerEvent.PlayerId);
+
+        // Act.
+        var foreignKey = _entity.GetForeignKeys()
+                                .SingleOrDefault(fk => fk.Properties.Any(p => p.Name == expected));
+
+        // Assert.
+        foreignKey.ShouldNotBeNull();
+        foreignKey.Properties.ShouldHaveSingleItem();
+        foreignKey.Properties[0].Name.ShouldBeEquivalentTo(expected);
+    }
+}
