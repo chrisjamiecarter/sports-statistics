9d1ab6765a7633cc45fda35886003f7444f9e6a5
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-18 18:47:20 +0000

    Updated entity and removed entity id

--- src/SportsStatistics.SharedKernel/Entity.cs
+++ src/SportsStatistics.SharedKernel/Entity.cs
@@ -1,10 +1,33 @@
 ﻿namespace SportsStatistics.SharedKernel;
 
-public abstract class Entity(EntityId id)
+public abstract class Entity
 {
     private readonly List<IDomainEvent> _domainEvents = [];
     
-    public EntityId Id { get; } = id;
+    protected Entity(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentException("Identifier value is required.", nameof(id));
+        }
+
+        if (id.Version != 7)
+        {
+            throw new ArgumentException("Identifier values must be version 7 GUIDs.", nameof(id));
+        }
+
+        Id = id;
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="Entity"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Required for Entity Framework Core.
+    /// </remarks>
+    protected Entity() { }
+
+    public Guid Id { get; private set; }
 
     public List<IDomainEvent> DomainEvents => [.. _domainEvents];
 
