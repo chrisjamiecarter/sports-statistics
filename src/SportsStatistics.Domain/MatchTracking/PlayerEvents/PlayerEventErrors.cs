87aa52ac7b2041452daac5b8852742ec017315c8
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-25 18:58:08 +0000

    Updated player events to be more DDD like

---
+++ src/SportsStatistics.Domain/MatchTracking/PlayerEvents/PlayerEventErrors.cs
@@ -0,0 +1,18 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.MatchTracking.PlayerEvents;
+
+public static class PlayerEventErrors
+{
+    public static Error FixtureIdIsRequired => Error.Validation(
+        "PlayerEvent.FixtureIdIsRequired",
+        "The fixture identifier is required.");
+
+    public static Error MinuteIsRequired => Error.Validation(
+        "PlayerEvent.MinuteIsRequired",
+        "The fixture identifier is required.");
+
+    public static Error OccurredAtDateAndTimeIsRequired => Error.Validation(
+        "PlayerEvent.OccurredAtDateAndTimeIsRequired",
+        "The occured at date and time of the player event is required.");
+}
--- src/SportsStatistics.Domain/MatchTracking/PlayerEvents/PlayerEventType.cs
+++ src/SportsStatistics.Domain/MatchTracking/PlayerEvents/PlayerEventType.cs
@@ -2,55 +2,29 @@
 
 namespace SportsStatistics.Domain.MatchTracking.PlayerEvents;
 
-public sealed class PlayerEventType : Enumeration
+public sealed class PlayerEventType : Enumeration<PlayerEventType>
 {
-    public static readonly PlayerEventType Unknown = new(0, nameof(Unknown));
-    public static readonly PlayerEventType PassSuccess = new(1, nameof(PassSuccess));
-    public static readonly PlayerEventType PassFailure = new(2, nameof(PassFailure));
-    public static readonly PlayerEventType ShotOnTarget = new(3, nameof(ShotOnTarget));
-    public static readonly PlayerEventType ShotOffTarget = new(4, nameof(ShotOffTarget));
-    public static readonly PlayerEventType Goal = new(5, nameof(Goal));
-    public static readonly PlayerEventType GoalAssist = new(6, nameof(GoalAssist));
-    public static readonly PlayerEventType OwnGoal = new(7, nameof(OwnGoal));
-    public static readonly PlayerEventType Save = new(8, nameof(Save));
-    public static readonly PlayerEventType Tackle = new(9, nameof(Tackle));
-    public static readonly PlayerEventType FoulWon = new(10, nameof(FoulWon));
-    public static readonly PlayerEventType FoulConceeded = new(11, nameof(FoulConceeded));
-    public static readonly PlayerEventType YellowCard = new(12, nameof(YellowCard));
-    public static readonly PlayerEventType RedCard = new(13, nameof(RedCard));
+    public static readonly PlayerEventType PassSuccess = new(0, nameof(PassSuccess));
+    public static readonly PlayerEventType PassFailure = new(1, nameof(PassFailure));
+    public static readonly PlayerEventType ShotOnTarget = new(2, nameof(ShotOnTarget));
+    public static readonly PlayerEventType ShotOffTarget = new(3, nameof(ShotOffTarget));
+    public static readonly PlayerEventType Goal = new(4, nameof(Goal));
+    public static readonly PlayerEventType GoalAssist = new(5, nameof(GoalAssist));
+    public static readonly PlayerEventType OwnGoal = new(6, nameof(OwnGoal));
+    public static readonly PlayerEventType Save = new(7, nameof(Save));
+    public static readonly PlayerEventType Tackle = new(8, nameof(Tackle));
+    public static readonly PlayerEventType FoulWon = new(9, nameof(FoulWon));
+    public static readonly PlayerEventType FoulConceeded = new(10, nameof(FoulConceeded));
+    public static readonly PlayerEventType YellowCard = new(11, nameof(YellowCard));
+    public static readonly PlayerEventType RedCard = new(12, nameof(RedCard));
 
     private PlayerEventType(int id, string name) : base(id, name) { }
 
-    public static IReadOnlyCollection<PlayerEventType> All =>
-    [
-        PassSuccess,
-        PassFailure,
-        ShotOnTarget,
-        ShotOffTarget,
-        Goal,
-        GoalAssist,
-        OwnGoal,
-        Save,
-        Tackle,
-        FoulWon,
-        FoulConceeded,
-        YellowCard,
-        RedCard
-    ];
-
-    public static int MaxLength => All.Max(type => type.Name.Length);
-
-    public static Result<PlayerEventType> Create(string value)
-    {
-        var resolvedValue =
-            All.SingleOrDefault(type => string.Equals(type.Name, value, StringComparison.OrdinalIgnoreCase))
-            ?? Unknown;
-
-        if (resolvedValue == Unknown)
-        {
-            return MatchTrackingErrors.PlayerEventType.Unknown;
-        }
-
-        return resolvedValue;
-    }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerEventType"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Required by EF Core.
+    /// </remarks>
+    private PlayerEventType() { }
 }
