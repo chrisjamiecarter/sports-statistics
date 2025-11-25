fc4e2a2cfffe63c7149a0d42fdcc489e4307f21c
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-25 18:57:57 +0000

    Updated match events to be more DDD like

--- src/SportsStatistics.Domain/MatchTracking/MatchEvents/MatchEventType.cs
+++ src/SportsStatistics.Domain/MatchTracking/MatchEvents/MatchEventType.cs
@@ -2,41 +2,22 @@
 
 namespace SportsStatistics.Domain.MatchTracking.MatchEvents;
 
-public class MatchEventType : Enumeration
+public class MatchEventType : Enumeration<MatchEventType>
 {
-    public static readonly MatchEventType Unknown = new(0, nameof(Unknown));
-    public static readonly MatchEventType FirstHalfStarted = new(1, nameof(FirstHalfStarted));
-    public static readonly MatchEventType FirstHalfFinished = new(2, nameof(FirstHalfFinished));
-    public static readonly MatchEventType SecondHalfStarted = new(3, nameof(SecondHalfStarted));
-    public static readonly MatchEventType SecondHalfFinished = new(4, nameof(SecondHalfFinished));
-    public static readonly MatchEventType HomeGoal = new(5, nameof(HomeGoal));
-    public static readonly MatchEventType AwayGoal = new(6, nameof(AwayGoal));
+    public static readonly MatchEventType FirstHalfStarted = new(0, nameof(FirstHalfStarted));
+    public static readonly MatchEventType FirstHalfFinished = new(1, nameof(FirstHalfFinished));
+    public static readonly MatchEventType SecondHalfStarted = new(2, nameof(SecondHalfStarted));
+    public static readonly MatchEventType SecondHalfFinished = new(3, nameof(SecondHalfFinished));
+    public static readonly MatchEventType HomeGoal = new(4, nameof(HomeGoal));
+    public static readonly MatchEventType AwayGoal = new(5, nameof(AwayGoal));
 
     private MatchEventType(int id, string name) : base(id, name) { }
 
-    public static IReadOnlyCollection<MatchEventType> All =>
-    [
-        FirstHalfStarted,
-        FirstHalfFinished,
-        SecondHalfStarted,
-        SecondHalfFinished,
-        HomeGoal,
-        AwayGoal
-    ];
-
-    public static int MaxLength => All.Max(type => type.Name.Length);
-
-    public static Result<MatchEventType> Create(string value)
-    {
-        var resolvedValue =
-            All.SingleOrDefault(type => string.Equals(type.Name, value, StringComparison.OrdinalIgnoreCase))
-            ?? Unknown;
-
-        if (resolvedValue == Unknown)
-        {
-            return MatchTrackingErrors.MatchEventType.Unknown;
-        }
-
-        return resolvedValue;
-    }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MatchEventType"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Required by EF Core.
+    /// </remarks>
+    private MatchEventType() { }
 }
