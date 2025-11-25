5b63a12da6fc49ccb1e46bddb53fd113e6f6b5e2
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-25 19:22:45 +0000

    Updated competition configuration to account for DDD properties

--- src/SportsStatistics.Infrastructure/Competitions/CompetitionConfiguration.cs
+++ src/SportsStatistics.Infrastructure/Competitions/CompetitionConfiguration.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SportsStatistics.Domain.Competitions;
 using SportsStatistics.Domain.Seasons;
 using SportsStatistics.Infrastructure.Database;
-using SportsStatistics.Infrastructure.Database.Converters;
 
 namespace SportsStatistics.Infrastructure.Competitions;
 
@@ -16,25 +15,36 @@ internal sealed class CompetitionConfiguration : IEntityTypeConfiguration<Compet
         builder.HasKey(competition => competition.Id);
 
         builder.Property(competition => competition.Id)
-               .HasConversion(Converters.EntityIdConverter)
                .IsRequired()
                .ValueGeneratedNever();
 
         builder.Property(competition => competition.SeasonId)
-               .HasConversion(Converters.EntityIdConverter)
                .IsRequired();
 
-        builder.Property(competition => competition.Name)
-               .HasMaxLength(50)
-               .IsRequired();
+        builder.ComplexProperty(competition => competition.Name, propertyBuilder =>
+        {
+            propertyBuilder.Property(name => name.Value)
+                           .HasMaxLength(Name.MaxLength)
+                           .IsRequired();
+        });
 
-        builder.Property(competition => competition.Type)
-               .HasConversion(Converters.CompetitionTypeConverter)
-               .HasMaxLength(CompetitionType.MaxLength)
-               .IsRequired();
+        builder.ComplexProperty(competition => competition.Format, propertyBuilder =>
+        {
+            propertyBuilder.Property(format => format.Value)
+                           .IsRequired();
+
+            propertyBuilder.Ignore(format => format.Name);
+        });
+
+        builder.Property(competition => competition.DeletedOnUtc);
+
+        builder.Property(competition => competition.Deleted)
+               .HasDefaultValue(false);
 
         builder.HasOne<Season>()
                .WithMany()
                .HasForeignKey(competition => competition.SeasonId);
+
+        builder.HasQueryFilter(competition => !competition.Deleted);
     }
 }
