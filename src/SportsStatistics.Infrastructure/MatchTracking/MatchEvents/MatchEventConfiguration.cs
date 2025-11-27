a7004c64fcb17c778b4a87286c9d9c0ce2951433
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-27 18:04:54 +0000

    Updated match event configuration to support DDD objects

--- src/SportsStatistics.Infrastructure/MatchTracking/MatchEvents/MatchEventConfiguration.cs
+++ src/SportsStatistics.Infrastructure/MatchTracking/MatchEvents/MatchEventConfiguration.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SportsStatistics.Domain.Fixtures;
 using SportsStatistics.Domain.MatchTracking.MatchEvents;
 using SportsStatistics.Infrastructure.Database;
-using SportsStatistics.Infrastructure.Database.Converters;
 
 namespace SportsStatistics.Infrastructure.MatchTracking.MatchEvents;
 
@@ -16,27 +15,33 @@ internal sealed class MatchEventConfiguration : IEntityTypeConfiguration<MatchEv
         builder.HasKey(matchEvent => matchEvent.Id);
 
         builder.Property(matchEvent => matchEvent.Id)
-               .HasConversion(Converters.EntityIdConverter)
                .IsRequired()
                .ValueGeneratedNever();
 
         builder.Property(matchEvent => matchEvent.FixtureId)
-               .HasConversion(Converters.EntityIdConverter)
                .IsRequired();
 
-        builder.Property(matchEvent => matchEvent.Minute)
-               .IsRequired();
+        builder.ComplexProperty(matchEvent => matchEvent.Minute, propertyBuilder =>
+        {
+            propertyBuilder.Property(minute => minute.Value)
+                           .IsRequired();
+        });
 
         builder.Property(matchEvent => matchEvent.OccurredAtUtc)
                .IsRequired();
 
         builder.Property(matchEvent => matchEvent.Type)
-               .HasConversion(Converters.MatchEventTypeConverter)
-               .HasMaxLength(MatchEventType.MaxLength)
                .IsRequired();
+
+        builder.Property(matchEvent => matchEvent.DeletedOnUtc);
+
+        builder.Property(matchEvent => matchEvent.Deleted)
+               .HasDefaultValue(false);
 
         builder.HasOne<Fixture>()
                .WithMany()
                .HasForeignKey(matchEvent => matchEvent.FixtureId);
+
+        builder.HasQueryFilter(matchEvent => !matchEvent.Deleted);
     }
 }
