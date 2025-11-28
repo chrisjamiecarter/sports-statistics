bfc50cd8dfd3de1ca38d10bc506f2348d3be5fbd
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-28 20:38:49 +0000

    Updated create competition command tests

--- tests/SportsStatistics.Application.Tests/Competitions/Create/CreateCompetitionCommandHandlerTests.cs
+++ tests/SportsStatistics.Application.Tests/Competitions/Create/CreateCompetitionCommandHandlerTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MockQueryable.Moq;
+﻿using MockQueryable.Moq;
 using SportsStatistics.Application.Abstractions.Data;
 using SportsStatistics.Application.Competitions.Create;
 using SportsStatistics.Domain.Competitions;
@@ -12,31 +11,28 @@ public class CreateCompetitionCommandHandlerTests
 {
     private static readonly List<Season> BaseSeasons =
     [
-        Season.Create(new DateOnly(2023, 8, 1), new DateOnly(2024, 7, 31)),
-        Season.Create(new DateOnly(2024, 8, 1), new DateOnly(2025, 7, 31)),
+        CompetitionFixtures.Season2023_2024,
+        CompetitionFixtures.Season2024_2025,
     ];
 
-    private static readonly CreateCompetitionCommand BaseCommand = new(BaseSeasons[0].Id,
-                                                                       "Premier League",
-                                                                       CompetitionType.League.Name);
+    private static readonly List<Competition> BaseCompetitions = [];
 
-    private readonly Mock<DbSet<Competition>> _competitionDbSetMock;
-    private readonly Mock<DbSet<Season>> _seasonDbSetMock;
+    private static readonly CreateCompetitionCommand BaseCommand = new(CompetitionFixtures.Season2023_2024.Id,
+                                                                       "Premier League",
+                                                                       Format.League.Value);
+
     private readonly Mock<IApplicationDbContext> _dbContextMock;
     private readonly CreateCompetitionCommandHandler _handler;
 
     public CreateCompetitionCommandHandlerTests()
     {
-        _competitionDbSetMock = new List<Competition>().BuildMockDbSet();
-        _seasonDbSetMock = BaseSeasons.BuildMockDbSet();
-
         _dbContextMock = new Mock<IApplicationDbContext>();
 
         _dbContextMock.Setup(m => m.Competitions)
-                      .Returns(_competitionDbSetMock.Object);
+                      .Returns(BaseCompetitions.BuildMockDbSet().Object);
 
         _dbContextMock.Setup(m => m.Seasons)
-                      .Returns(_seasonDbSetMock.Object);
+                      .Returns(BaseSeasons.BuildMockDbSet().Object);
 
         _dbContextMock.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
                       .ReturnsAsync(1);
@@ -63,24 +59,7 @@ public class CreateCompetitionCommandHandlerTests
     {
         // Arrange.
         var command = BaseCommand with { SeasonId = Guid.CreateVersion7() };
-        var expected = Result.Failure(SeasonErrors.NotFound(EntityId.Create(command.SeasonId)));
-
-        // Act.
-        var result = await _handler.Handle(command, CancellationToken.None);
-
-        // Assert.
-        result.ShouldBeEquivalentTo(expected);
-    }
-
-    [Fact]
-    public async Task Handle_ShouldReturnFailure_WhenCompetitionIsNotCreated()
-    {
-        // Arrange.
-        var command = BaseCommand;
-        var expected = Result.Failure(CompetitionErrors.NotCreated(command.Name, command.CompetitionTypeName));
-
-        _dbContextMock.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
-                      .ReturnsAsync(0);
+        var expected = Result.Failure(SeasonErrors.NotFound(command.SeasonId));
 
         // Act.
         var result = await _handler.Handle(command, CancellationToken.None);
--- tests/SportsStatistics.Application.Tests/Competitions/Create/CreateCompetitionCommandValidatorTests.cs
+++ tests/SportsStatistics.Application.Tests/Competitions/Create/CreateCompetitionCommandValidatorTests.cs
@@ -8,7 +8,7 @@ public class CreateCompetitionCommandValidatorTests
 {
     private static readonly CreateCompetitionCommand BaseCommand = new(Guid.CreateVersion7(),
                                                                        "Test Name",
-                                                                       CompetitionType.League.Name);
+                                                                       Format.League.Value);
 
     private readonly CreateCompetitionCommandValidator _validator;
 
@@ -78,50 +78,49 @@ public class CreateCompetitionCommandValidatorTests
               .WithErrorMessage(expected);
     }
 
-    [Theory]
-    [InlineData("")]
-    [InlineData("    ")]
-    public async Task ValidateAsync_ShouldHaveValidationError_WhenCompetitionTypeNameIsEmpty(string competitionTypeName)
+    [Fact]
+    public async Task ValidateAsync_ShouldHaveValidationError_WhenFormatIdIsEmpty()
     {
         // Arrange.
-        var command = BaseCommand with { CompetitionTypeName = competitionTypeName };
+        var command = BaseCommand with { FormatId = default };
         var expected = "'Competition Type Name' must not be empty.";
 
         // Act.
         var result = await _validator.TestValidateAsync(command);
 
         // Assert.
-        result.ShouldHaveValidationErrorFor(c => c.CompetitionTypeName)
+        result.ShouldHaveValidationErrorFor(c => c.FormatId)
               .WithErrorMessage(expected);
     }
 
-    [Theory]
-    [InlineData("League")]
-    [InlineData("Cup")]
-    public async Task ValidateAsync_ShouldNotHaveAnyValidationErrors_WhenCompetitionTypeNameIsValid(string competitionTypeName)
+    [Fact]
+    public async Task ValidateAsync_ShouldNotHaveAnyValidationErrors_WhenFormatIdIsValid()
     {
-        // Arrange.
-        var command = BaseCommand with { CompetitionTypeName = competitionTypeName };
+        foreach (var format in Format.List)
+        {
+            // Arrange.
+            var command = BaseCommand with { FormatId = format.Value };
 
-        // Act.
-        var result = await _validator.TestValidateAsync(command);
+            // Act.
+            var result = await _validator.TestValidateAsync(command);
 
-        // Assert.
-        result.ShouldNotHaveAnyValidationErrors();
+            // Assert.
+            result.ShouldNotHaveAnyValidationErrors();
+        }
     }
 
     [Fact]
-    public async Task ValidateAsync_ShouldHaveValidationError_WhenCompetitionTypeNameIsInvalid()
+    public async Task ValidateAsync_ShouldHaveValidationError_WhenFormatIdIsInvalid()
     {
         // Arrange.
-        var command = BaseCommand with { CompetitionTypeName = "Training" };
-        var expected = $"'Competition Type Name' is invalid. Valid options: {string.Join(", ", CompetitionType.All)}.";
+        var command = BaseCommand with { FormatId = -1 };
+        var expected = "'Competition Type Name' is invalid.";
 
         // Act.
         var result = await _validator.TestValidateAsync(command);
 
         // Assert.
-        result.ShouldHaveValidationErrorFor(c => c.CompetitionTypeName)
+        result.ShouldHaveValidationErrorFor(c => c.FormatId)
               .WithErrorMessage(expected);
     }
 }
