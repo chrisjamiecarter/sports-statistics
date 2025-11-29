acea3d3b071fd719caf48cfc1911f17af60037eb
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-29 11:49:13 +0000

    Added season fixtures

---
+++ tests/SportsStatistics.Application.Tests/Seasons/SeasonFixtures.cs
@@ -0,0 +1,12 @@
+﻿using SportsStatistics.Domain.Seasons;
+
+namespace SportsStatistics.Application.Tests.Seasons;
+
+public static class SeasonFixtures
+{
+    public static readonly Season Season2023_2024 =
+        Season.Create(DateRange.Create(new(2023, 8, 1), new(2024, 7, 31)).Value);
+
+    public static readonly Season Season2024_2025 =
+        Season.Create(DateRange.Create(new(2024, 8, 1), new(2025, 7, 31)).Value);
+}
