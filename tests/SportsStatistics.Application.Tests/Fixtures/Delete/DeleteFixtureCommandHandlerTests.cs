8b397b3c00a25181ff1106cf17feb2db896c9537
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-29 11:29:11 +0000

    Updated delete fixture command tests

--- tests/SportsStatistics.Application.Tests/Fixtures/Delete/DeleteFixtureCommandHandlerTests.cs
+++ tests/SportsStatistics.Application.Tests/Fixtures/Delete/DeleteFixtureCommandHandlerTests.cs
@@ -11,23 +11,20 @@ public class DeleteFixtureCommandHandlerTests
 {
     private static readonly List<Fixture> BaseFixtures =
     [
-        Fixture.Create(EntityId.Create(), "Test Opponent", DateTime.UtcNow, FixtureLocation.Home.Name),
+        FixtureFixtures.FixtureGW1League2024_2925,
     ];
 
-    private static readonly DeleteFixtureCommand BaseCommand = new(BaseFixtures[0].Id);
+    private static readonly DeleteFixtureCommand BaseCommand = new(FixtureFixtures.FixtureGW1League2024_2925.Id);
 
-    private readonly Mock<DbSet<Fixture>> _fixtureDbSetMock;
     private readonly Mock<IApplicationDbContext> _dbContextMock;
     private readonly DeleteFixtureCommandHandler _handler;
 
     public DeleteFixtureCommandHandlerTests()
     {
-        _fixtureDbSetMock = BaseFixtures.BuildMockDbSet();
-
         _dbContextMock = new Mock<IApplicationDbContext>();
 
         _dbContextMock.Setup(m => m.Fixtures)
-                      .Returns(_fixtureDbSetMock.Object);
+                      .Returns(BaseFixtures.BuildMockDbSet().Object);
 
         _dbContextMock.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
                       .ReturnsAsync(1);
@@ -55,23 +52,6 @@ public class DeleteFixtureCommandHandlerTests
         // Arrange.
         var command = BaseCommand with { FixtureId = Guid.CreateVersion7() };
         var expected = Result.Failure(FixtureErrors.NotFound(command.FixtureId));
-
-        // Act.
-        var result = await _handler.Handle(command, CancellationToken.None);
-
-        // Assert.
-        result.ShouldBeEquivalentTo(expected);
-    }
-
-    [Fact]
-    public async Task Handle_ShouldReturnFailure_WhenFixtureIsNotDeleted()
-    {
-        // Arrange.
-        var command = BaseCommand;
-        var expected = Result.Failure(FixtureErrors.NotDeleted(command.FixtureId));
-
-        _dbContextMock.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
-                      .ReturnsAsync(0);
 
         // Act.
         var result = await _handler.Handle(command, CancellationToken.None);
