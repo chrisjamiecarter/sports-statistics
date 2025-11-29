6c88bfeebc99e1fd5de9b1189331b07e7ae669d4
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-11-29 11:52:16 +0000

    Updated delete season command tests

--- tests/SportsStatistics.Application.Tests/Seasons/Delete/DeleteSeasonCommandHandlerTests.cs
+++ tests/SportsStatistics.Application.Tests/Seasons/Delete/DeleteSeasonCommandHandlerTests.cs
@@ -10,11 +10,11 @@ public class DeleteSeasonCommandHandlerTests
 {
     private static readonly List<Season> BaseSeasons =
     [
-        Season.Create(new DateOnly(2023, 8, 1), new DateOnly(2024, 7, 31)),
-        Season.Create(new DateOnly(2024, 8, 1), new DateOnly(2025, 7, 31)),
+        SeasonFixtures.Season2023_2024,
+        SeasonFixtures.Season2024_2025
     ];
 
-    private static readonly DeleteSeasonCommand BaseCommand = new(BaseSeasons[0].Id);
+    private static readonly DeleteSeasonCommand BaseCommand = new(SeasonFixtures.Season2023_2024.Id);
 
     private readonly Mock<IApplicationDbContext> _dbContextMock;
     private readonly DeleteSeasonCommandHandler _handler;
@@ -52,23 +52,6 @@ public class DeleteSeasonCommandHandlerTests
         // Arrange.
         var command = BaseCommand with { SeasonId = Guid.CreateVersion7() };
         var expected = Result.Failure(SeasonErrors.NotFound(command.SeasonId));
-
-        // Act.
-        var result = await _handler.Handle(command, CancellationToken.None);
-
-        // Assert.
-        result.ShouldBeEquivalentTo(expected);
-    }
-
-    [Fact]
-    public async Task Handle_ShouldReturnFailure_WhenSeasonIsNotDeleted()
-    {
-        // Arrange.
-        var command = BaseCommand;
-        var expected = Result.Failure(SeasonErrors.NotDeleted(command.SeasonId));
-
-        _dbContextMock.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
-                      .ReturnsAsync(0);
 
         // Act.
         var result = await _handler.Handle(command, CancellationToken.None);
