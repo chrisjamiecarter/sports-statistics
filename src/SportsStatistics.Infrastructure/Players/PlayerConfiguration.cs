8d4555670011150b53f40b6ed68527cc209de19f
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-01 18:26:30 +0000

    Updated player configuration to include column name

--- src/SportsStatistics.Infrastructure/Players/PlayerConfiguration.cs
+++ src/SportsStatistics.Infrastructure/Players/PlayerConfiguration.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SportsStatistics.Domain.Players;
-using SportsStatistics.Domain.Seasons;
 using SportsStatistics.Infrastructure.Database;
-using SportsStatistics.Infrastructure.Database.Converters;
 
 namespace SportsStatistics.Infrastructure.Players;
 
@@ -16,44 +14,51 @@ internal sealed class PlayerConfiguration : IEntityTypeConfiguration<Player>
         builder.HasKey(player => player.Id);
 
         builder.Property(player => player.Id)
+               .HasColumnName(nameof(Player.Id))
                .IsRequired()
                .ValueGeneratedNever();
 
-        builder.ComplexProperty(player => player.Name, propertyBuilder =>
+        builder.ComplexProperty(player => player.Name, complexBuilder =>
         {
-            propertyBuilder.Property(name => name.Value)
-                           .HasMaxLength(Name.MaxLength)
-                           .IsRequired();
+            complexBuilder.Property(name => name.Value)
+                          .HasColumnName(nameof(Player.Name))
+                          .HasMaxLength(Name.MaxLength)
+                          .IsRequired();
         });
 
-        builder.ComplexProperty(player => player.SquadNumber, propertyBuilder =>
+        builder.ComplexProperty(player => player.SquadNumber, complexBuilder =>
         {
-            propertyBuilder.Property(squadNumber => squadNumber.Value)
-                           .IsRequired();
+            complexBuilder.Property(squadNumber => squadNumber.Value)
+                          .HasColumnName(nameof(Player.SquadNumber))
+                          .IsRequired();
         });
 
-        builder.ComplexProperty(player => player.Nationality, propertyBuilder =>
+        builder.ComplexProperty(player => player.Nationality, complexBuilder =>
         {
-            propertyBuilder.Property(nationality => nationality.Value)
-                           .HasMaxLength(Nationality.MaxLength)
-                           .IsRequired();
+            complexBuilder.Property(nationality => nationality.Value)
+                          .HasColumnName(nameof(Player.Nationality))
+                          .HasMaxLength(Nationality.MaxLength)
+                          .IsRequired();
         });
 
-        builder.ComplexProperty(player => player.DateOfBirth, propertyBuilder =>
+        builder.ComplexProperty(player => player.DateOfBirth, complexBuilder =>
         {
-            propertyBuilder.Property(dateOfBirth => dateOfBirth.Value)
-                           //.HasColumnType("date")
-                           .IsRequired();
+            complexBuilder.Property(dateOfBirth => dateOfBirth.Value)
+                          .HasColumnName(nameof(Player.DateOfBirth))
+                          .IsRequired();
         });
 
         builder.Property(player => player.Position)
+               .HasColumnName(nameof(Player.Position))
                .IsRequired();
 
         builder.Ignore(player => player.Age);
 
-        builder.Property(player => player.DeletedOnUtc);
+        builder.Property(player => player.DeletedOnUtc)
+               .HasColumnName(nameof(Player.DeletedOnUtc));
 
         builder.Property(player => player.Deleted)
+               .HasColumnName(nameof(Player.Deleted))
                .HasDefaultValue(false);
 
         builder.HasQueryFilter(player => !player.Deleted);
