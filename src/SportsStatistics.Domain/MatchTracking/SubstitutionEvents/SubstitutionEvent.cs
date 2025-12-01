812a2a71f92ad51bcaf1a0e9c9649ab8dd8a5bf3
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-01 18:24:46 +0000

    Added missing empty constructor to substitution event for efcore

--- src/SportsStatistics.Domain/MatchTracking/SubstitutionEvents/SubstitutionEvent.cs
+++ src/SportsStatistics.Domain/MatchTracking/SubstitutionEvents/SubstitutionEvent.cs
@@ -11,6 +11,14 @@ public class SubstitutionEvent : MatchEventBase
         Substitution = substitution;
     }
 
+    /// <summary>
+    /// Initialises a new instance of the <see cref="SubstitutionEvent"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Required for Entity Framework Core.
+    /// </remarks>
+    private SubstitutionEvent() { }
+
     public Substitution Substitution { get; private set; } = default!;
 
     public static SubstitutionEvent Create(Guid fixtureId, Substitution substitution, Minute minute, DateTime occurredAtUtc)
