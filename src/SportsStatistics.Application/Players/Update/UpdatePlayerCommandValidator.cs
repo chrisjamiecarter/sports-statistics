b3ba74a7693d2e0e0dbcd5a5ddae9db37a6e4cf7
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-04 22:29:27 +0000

    Corrected the update player command validator for when validating a valid position id

--- src/SportsStatistics.Application/Players/Update/UpdatePlayerCommandValidator.cs
+++ src/SportsStatistics.Application/Players/Update/UpdatePlayerCommandValidator.cs
@@ -27,7 +27,6 @@ internal sealed class UpdatePlayerCommandValidator : AbstractValidator<UpdatePla
             .Must(dob => dob.CalculateAge() >= DateOfBirth.MinAge).WithError(PlayerErrors.DateOfBirthBelowMinAge);
 
         RuleFor(c => c.PositionId)
-            .NotEmpty().WithError(PlayerErrors.PositionIdIsRequired)
-            .Must(positionId => Position.Resolve(positionId).IsSuccess).WithError(PlayerErrors.PositionNotFound);
+            .Must(Position.ContainsValue).WithError(PlayerErrors.PositionNotFound);
     }
 }
