dac6963653145567e03c1bcf2545f682a54a37c8
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-06 16:14:45 +0000

    Added player test data

---
+++ tests/SportsStatistics.Domain.Tests/Players/PlayerTestData.cs
@@ -0,0 +1,18 @@
+﻿using SportsStatistics.Domain.Players;
+
+namespace SportsStatistics.Domain.Tests.Players;
+
+public static class PlayerTestData
+{
+    public static readonly Name Name = Name.Create("Name").Value;
+
+    public static readonly SquadNumber SquadNumber = SquadNumber.Create(1).Value;
+
+    public static readonly Nationality Nationality = Nationality.Create("Nationality").Value;
+
+    public static readonly DateOfBirth DateOfBirth = DateOfBirth.Create(new(2000, 1, 1)).Value;
+
+    public static readonly Position Position = Position.Goalkeeper;
+
+    public static Player ValidPlayer => Player.Create(Name, SquadNumber, Nationality, DateOfBirth, Position);
+}
