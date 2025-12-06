30a4374edc7a924fad037fc966e26ba1662cba61
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-06 16:14:53 +0000

    Added player tests

---
+++ tests/SportsStatistics.Domain.Tests/Players/PlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using SportsStatistics.Domain.Players;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Tests.Players;
+
+public class PlayerTests
+{
+    [Fact]
+    public void Create_ShouldCreatePlayer_WhenParametersAreValid()
+    {
+        // Arrange.
+        // Act.
+        var player = Player.Create(PlayerTestData.Name,
+                                   PlayerTestData.SquadNumber,
+                                   PlayerTestData.Nationality,
+                                   PlayerTestData.DateOfBirth,
+                                   PlayerTestData.Position);
+
+        // Assert.
+        player.ShouldNotBeNull();
+        player.Id.ShouldNotBe(default);
+        player.Id.Version.ShouldBe(7);
+        player.Name.ShouldBe(PlayerTestData.Name);
+        player.SquadNumber.ShouldBe(PlayerTestData.SquadNumber);
+        player.Nationality.ShouldBe(PlayerTestData.Nationality);
+        player.DateOfBirth.ShouldBe(PlayerTestData.DateOfBirth);
+        player.Position.ShouldBe(PlayerTestData.Position);
+        player.Age.ShouldBe(PlayerTestData.DateOfBirth.Value.CalculateAge());
+        player.Deleted.ShouldBeFalse();
+        player.DeletedOnUtc.ShouldBeNull();
+    }
+}
