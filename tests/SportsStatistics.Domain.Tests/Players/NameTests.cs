44dd2057b00305b276b5b3f62edee11df7825414
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-07 20:15:00 +0000

    Added name tests

---
+++ tests/SportsStatistics.Domain.Tests/Players/NameTests.cs
@@ -0,0 +1,37 @@
+﻿using SportsStatistics.Domain.Players;
+using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Tests.Players;
+
+public class NameTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Create_ShouldReturnFailureResult_WhenNameIsNullOrEmpty(string? name)
+    {
+        // Arrange.
+        var expected = Result.Failure<Name>(PlayerErrors.Name.NullOrEmpty);
+
+        // Act.
+        var result = Name.Create(name);
+
+        // Assert.
+        result.Error.ShouldBeEquivalentTo(expected.Error);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnFailureResult_WhenNameIsTooLong()
+    {
+        // Arrange.
+        var name = NameTestData.LongerThanAllowedName;
+        var expected = Result.Failure<Name>(PlayerErrors.Name.ExceedsMaxLength);
+
+        // Act.
+        var result = Name.Create(name);
+
+        // Assert.
+        result.Error.ShouldBeEquivalentTo(expected.Error);
+    }
+}
