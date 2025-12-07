e98cbcfd52ffc548953e9bd0d1d52fec08d0b060
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-07 20:14:30 +0000

    Added name test data

---
+++ tests/SportsStatistics.Domain.Tests/Players/NameTestData.cs
@@ -0,0 +1,10 @@
+﻿using SportsStatistics.Domain.Players;
+
+namespace SportsStatistics.Domain.Tests.Players;
+
+public class NameTestData
+{
+    public static readonly Name ValidName = Name.Create(nameof(Name)).Value;
+
+    public static readonly string LongerThanAllowedName = new('*', Name.MaxLength + 1);
+}
