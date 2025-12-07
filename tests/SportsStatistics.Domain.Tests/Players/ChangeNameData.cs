6e37d04b653d0bfcca4fe42090965c960171247f
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-07 20:14:17 +0000

    Added change name test data

---
+++ tests/SportsStatistics.Domain.Tests/Players/ChangeNameData.cs
@@ -0,0 +1,8 @@
+﻿using SportsStatistics.Domain.Players;
+
+namespace SportsStatistics.Domain.Tests.Players;
+
+public class ChangeNameData : TheoryData<Name, Name>
+{
+    public ChangeNameData() => Add(Name.Create("Old Name").Value, Name.Create("New Name").Value);
+}
