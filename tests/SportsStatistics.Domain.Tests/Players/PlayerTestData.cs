b71f934b153c8e8e1610ca3b9cadbf38cf12aeae
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-07 20:14:41 +0000

    Added player tests data

--- tests/SportsStatistics.Domain.Tests/Players/PlayerTestData.cs
+++ tests/SportsStatistics.Domain.Tests/Players/PlayerTestData.cs
@@ -4,9 +4,9 @@ namespace SportsStatistics.Domain.Tests.Players;
 
 public static class PlayerTestData
 {
-    public static readonly Name Name = Name.Create("Name").Value;
+    public static readonly Name Name = NameTestData.ValidName;
 
-    public static readonly SquadNumber SquadNumber = SquadNumber.Create(1).Value;
+    public static readonly SquadNumber SquadNumber = SquadNumberTestData.ValidSquadNumber;
 
     public static readonly Nationality Nationality = Nationality.Create("Nationality").Value;
 
@@ -15,4 +15,6 @@ public static class PlayerTestData
     public static readonly Position Position = Position.Goalkeeper;
 
     public static Player ValidPlayer => Player.Create(Name, SquadNumber, Nationality, DateOfBirth, Position);
+
+    public static Player ValidPlayerWithName(Name name) => Player.Create(name, SquadNumber, Nationality, DateOfBirth, Position);
 }
