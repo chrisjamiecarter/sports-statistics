0f7033aebcc524ff2495384f48e24cbca9c58bf3
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-13 18:15:20 +0000

    Added competition test data

---
+++ tests/SportsStatistics.Domain.Tests/Competitions/TestData/CompetitionTestData.cs
@@ -0,0 +1,16 @@
+﻿using SportsStatistics.Domain.Competitions;
+using SportsStatistics.Domain.Seasons;
+using SportsStatistics.Domain.Tests.Seasons.TestData;
+
+namespace SportsStatistics.Domain.Tests.Competitions.TestData;
+
+public static class CompetitionTestData
+{
+    public static Season Season => SeasonTestData.ValidSeason;
+
+    public static Name Name => NameTestData.ValidName;
+
+    public static Format Format => FormatTestData.ValidFormat;
+
+    public static Competition ValidCompetition => Competition.Create(Season, Name, Format);
+}
---
+++ tests/SportsStatistics.Domain.Tests/Competitions/TestData/FormatTestData.cs
@@ -0,0 +1,8 @@
+﻿using SportsStatistics.Domain.Competitions;
+
+namespace SportsStatistics.Domain.Tests.Competitions.TestData;
+
+public static class FormatTestData
+{
+    public static readonly Format ValidFormat = Format.League;
+}
