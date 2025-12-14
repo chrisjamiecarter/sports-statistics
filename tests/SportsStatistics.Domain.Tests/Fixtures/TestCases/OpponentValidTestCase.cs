52ecf36c38f81726d80aa435156c9fa6f80903c4
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-14 18:21:26 +0000

    Added fixture test cases

---
+++ tests/SportsStatistics.Domain.Tests/Fixtures/TestCases/OpponentValidTestCase.cs
@@ -0,0 +1,13 @@
+﻿using SportsStatistics.Domain.Fixtures;
+using SportsStatistics.Domain.Tests.Fixtures.TestData;
+
+namespace SportsStatistics.Domain.Tests.Fixtures.TestCases;
+
+public class OpponentValidTestCase : TheoryData<string, Opponent>
+{
+    private static readonly Opponent Opponent = OpponentTestData.ValidOpponent;
+    public OpponentValidTestCase()
+    {
+        Add(Opponent.Value, Opponent);
+    }
+}
