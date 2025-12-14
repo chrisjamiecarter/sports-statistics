da8d5dd181c7b224fd59c103035d16d98bd4a9cc
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-14 18:21:20 +0000

    Added fixture test data

---
+++ tests/SportsStatistics.Domain.Tests/Fixtures/TestData/FixtureTestData.cs
@@ -0,0 +1,12 @@
+﻿using SportsStatistics.Domain.Fixtures;
+using SportsStatistics.Domain.Tests.Competitions.TestData;
+
+namespace SportsStatistics.Domain.Tests.Fixtures.TestData;
+
+public static class FixtureTestData
+{
+    public static Fixture ValidFixture => Fixture.Create(CompetitionTestData.ValidCompetition,
+                                                          OpponentTestData.ValidOpponent,
+                                                          KickoffTimeUtcTestData.ValidKickoffTimeUtc,
+                                                          LocationTestData.ValidLocation);
+}
---
+++ tests/SportsStatistics.Domain.Tests/Fixtures/TestData/LocationTestData.cs
@@ -0,0 +1,8 @@
+﻿using SportsStatistics.Domain.Fixtures;
+
+namespace SportsStatistics.Domain.Tests.Fixtures.TestData;
+
+public static class LocationTestData
+{
+    public static readonly Location ValidLocation = Location.Home;
+}
