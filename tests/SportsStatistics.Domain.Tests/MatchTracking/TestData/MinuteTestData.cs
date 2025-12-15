dbd796fba7a0cf8a5716d2698aa571ff314485ef
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-15 19:15:25 +0000

    Added match tracking test data

---
+++ tests/SportsStatistics.Domain.Tests/MatchTracking/TestData/MinuteTestData.cs
@@ -0,0 +1,12 @@
+﻿using SportsStatistics.Domain.MatchTracking;
+
+namespace SportsStatistics.Domain.Tests.MatchTracking.TestData;
+
+public static class MinuteTestData
+{
+    public static Minute ValidMinute => Minute.Create(Minute.MinValue).Value;
+
+    public static readonly int BelowMinValueMinute = Minute.MinValue - 1;
+
+    public static readonly int? NullMinute;
+}
