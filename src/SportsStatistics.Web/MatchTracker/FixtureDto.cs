7fd0ead479bbbbb261a55980f14210aa3248b40a
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-16 17:48:50 +0000

    Implemented basic fixture selector as part of the match tracking feature

---
+++ src/SportsStatistics.Web/MatchTracker/FixtureDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SportsStatistics.Web.MatchTracker;
+
+public sealed record FixtureDto(Guid Id,
+                                Guid CompetitionId,
+                                string CompetitionName,
+                                string Opponent,
+                                DateTime KickoffTimeUtc,
+                                int LocationId,
+                                string Location,
+                                int HomeGoals,
+                                int AwayGoals,
+                                int StatusId,
+                                string Status)
+{
+    public override string ToString() => $"{KickoffTimeUtc:yyyy-MM-dd HH:mm} - {Opponent} - {CompetitionName}";
+}
