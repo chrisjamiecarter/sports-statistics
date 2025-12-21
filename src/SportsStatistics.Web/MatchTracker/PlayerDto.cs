ab98361d9e412f2ae79fbdc7a268ced4a70688a3
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-12-21 20:01:28 +0000

    Implemented teamsheet selector draft

---
+++ src/SportsStatistics.Web/MatchTracker/PlayerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsStatistics.Web.MatchTracker;
+
+public sealed record PlayerDto(Guid Id,
+                               string Name,
+                               int SquadNumber,
+                               string Nationality,
+                               DateOnly DateOfBirth,
+                               int PostionId,
+                               string Position,
+                               int Age);
---
+++ src/SportsStatistics.Web/MatchTracker/PlayerMapper.cs
@@ -0,0 +1,29 @@
+﻿using SportsStatistics.Application.Players.GetAll;
+
+namespace SportsStatistics.Web.MatchTracker;
+
+internal static class PlayerMapper
+{
+    public static PlayerDto ToDto(this PlayerResponse player)
+        => new(player.Id,
+               player.Name,
+               player.SquadNumber,
+               player.Nationality,
+               player.DateOfBirth,
+               player.PositionId,
+               player.Position,
+               player.Age);
+
+    //public static PlayerFormModel ToInputModel(this PlayerDto player, IEnumerable<PositionOptionDto> positionOptions)
+    //    => new()
+    //    {
+    //        Name = player.Name,
+    //        SquadNumber = player.SquadNumber,
+    //        Nationality = player.Nationality,
+    //        DateOfBirth = player.DateOfBirth.ToDateTime(),
+    //        Position = positionOptions.SingleOrDefault(option => option.Value == player.PostionId),
+    //    };
+
+    //public static IQueryable<PlayerDto> ToQueryable(this List<PlayerResponse> players)
+    //    => players.Select(ToDto).AsQueryable();
+}
