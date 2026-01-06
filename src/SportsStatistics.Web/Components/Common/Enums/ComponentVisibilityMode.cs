0b70c3fe6b0ee70cad93b12630f1006ababd02d1
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-01-06 19:32:05 +0000

    Added initial fixture upload component for implementation

---
+++ src/SportsStatistics.Web/Components/Common/Enums/ComponentVisibilityMode.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsStatistics.Web.Components.Common.Enums;
+
+/// <summary>
+/// Defines the available modes for component visibility.
+/// </summary>
+public enum ComponentVisibilityMode
+{
+    Hide,
+    Show,
+}
