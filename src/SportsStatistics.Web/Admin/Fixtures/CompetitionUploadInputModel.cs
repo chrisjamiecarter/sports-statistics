7056e25bbd7368638b07931482b1fe46e5c75630
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-01-09 17:37:13 +0000

    Updated competition upload input model to hold the format id instead of the options as it is not designed to be changable.

--- src/SportsStatistics.Web/Admin/Fixtures/CompetitionUploadInputModel.cs
+++ src/SportsStatistics.Web/Admin/Fixtures/CompetitionUploadInputModel.cs
@@ -5,6 +5,6 @@ namespace SportsStatistics.Web.Admin.Fixtures;
 public sealed class CompetitionUploadInputModel
 {
     public string? Name { get; set; }
-    public FormatOptionDto? Format { get; set; }
+    public int? FormatId { get; set; }
     public List<FixtureUploadInputModel> Fixtures { get; set; } = [];
 }
