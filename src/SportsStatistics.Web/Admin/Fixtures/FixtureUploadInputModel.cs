9f4a59b35fe9c554248bd21f678d92f924f8721c
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-01-09 17:39:02 +0000

    Added additional fields to the fixture upload input model so that relevant data can be displayed to aid input

--- src/SportsStatistics.Web/Admin/Fixtures/FixtureUploadInputModel.cs
+++ src/SportsStatistics.Web/Admin/Fixtures/FixtureUploadInputModel.cs
@@ -2,7 +2,10 @@
 
 public sealed class FixtureUploadInputModel
 {
+    public string? Season { get; set; }
+    public string? Competition { get; set; }
     public string? Opponent { get; set; }
+    public DateTime? KickoffDateUtc { get; set; }
     public DateTime? KickoffTimeUtc { get; set; }
     public LocationOptionDto? Location { get; set; }
 }
