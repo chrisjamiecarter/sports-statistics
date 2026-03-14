9494f0d48468bda8918b8dc698c8e9258933b0bd
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-03-14 22:03:28 +0000

    Added club domain objects

---
+++ src/SportsStatistics.Domain/Clubs/ClubErrors.cs
@@ -0,0 +1,17 @@
+﻿using SportsStatistics.SharedKernel;
+
+namespace SportsStatistics.Domain.Clubs;
+
+public static class ClubErrors
+{
+    public static class Name
+    {
+        public static Error ExceedsMaxLength => Error.Validation(
+            "Club.Name.ExceedsMaxLength",
+            $"The club name must be {Clubs.Name.MaxLength} characters or less.");
+
+        public static Error NullOrEmpty => Error.Validation(
+            "Club.Name.NullOrEmpty",
+            "The club name cannot be null or empty.");
+    }
+}
